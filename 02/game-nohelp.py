1a792cb3cbc1f08dfe731c881f7cf4f924ec7ef0
pybites <pybites@projects.bobbelderbos.com>
2017-01-13 02:15:33 +0100

    most advanced template

---
+++ 02/game-nohelp.py
@@ -0,0 +1,13 @@
+#!python3
+# Code Challenge 02 - Word Values Part II - a simple game
+# http://pybit.es/codechallenge02.html
+
+from data import DICTIONARY, LETTER_VALUES, POUNCH
+
+NUM_LETTERS = 7
+
+def main():
+    pass
+
+if __name__ == "__main__":
+    main()
