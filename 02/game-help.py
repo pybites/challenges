77df2bf28b617eb95696c513254042edf69f15a9
pybites <pybites@projects.bobbelderbos.com>
2017-01-15 13:28:14 +0100

    update game-help template

--- 02/game-help.py
+++ 02/game-help.py
@@ -10,35 +10,47 @@ from data import DICTIONARY, LETTER_SCORES, POUCH
 NUM_LETTERS = 7
 
 
-def calc_word_value(word):
-    """From challenge 01:
-    Calc a given word value based on Scrabble LETTER_SCORES mapping"""
-    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
-
-
-def max_word_value(words):
-    """From challenge 01:
-    Calc the max value of a collection of words"""
-    return max(words, key=lambda w: calc_word_value(w))
-
-
 def draw_letters():
     """Pick NUM_LETTERS letters randomly. Hint: use stdlib random"""
     pass
 
 
 def input_word(draw):
-    """Ask player for a word. Validations: 1) only use letters of draw, 2) valid dictionary word"""
+    """Ask player for a word.
+    Validations: 1) only use letters of draw, 2) valid dictionary word"""
     pass
 
 
+# From challenge 01:
+def calc_word_value(word):
+    """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+
+
+# Below 2 functions pass through the same 'draw' argument (smell?).
+# Maybe you want to abstract this into a class?
+# get_possible_dict_words and _get_permutations_draw would be instance methods.
+# 'draw' would be set in the class constructor (__init__).
 def get_possible_dict_words(draw):
-    """Generate a collection of all possible words in DICTIONARY (set) from draw of NUM_LETTERS
+    """Get all possible words from draw which are valid dictionary words.
+    Use the _get_permutations_draw helper and DICTIONARY constant"""
+    pass
+
+
+def _get_permutations_draw(draw):
+    """Helper for get_possible_dict_words to get all permutations of draw letters.
     Hint: use itertools.permutations"""
     pass
 
 
+# From challenge 01:
+def max_word_value(words):
+    """Calc the max value of a collection of words"""
+    return max(words, key=lambda w: calc_word_value(w))
+
+
 def main():
+    """Main game interface calling the previously defined methods"""
     draw = draw_letters()
     print('Letters drawn: {}'.format(', '.join(draw)))
 
@@ -50,7 +62,8 @@ def main():
 
     max_word = max_word_value(possible_words)
     max_word_score = calc_word_value(max_word)
-    print('Optimal word possible: {} (value: {})'.format(max_word, max_word_score))
+    print('Optimal word possible: {} (value: {})'.format(
+        max_word, max_word_score))
 
     game_score = word_score / max_word_score * 100
     print('You scored: {:.1f}'.format(game_score))
