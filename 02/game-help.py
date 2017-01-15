015958af887af9b45e60bbbace959daf9425a9b3
pybites <pybites@projects.bobbelderbos.com>
2017-01-15 13:08:08 +0100

    update help template

--- 02/game-help.py
+++ 02/game-help.py
@@ -10,32 +10,31 @@ from data import DICTIONARY, LETTER_SCORES, POUCH
 NUM_LETTERS = 7
 
 
-# re-use from challenge 01
 def calc_word_value(word):
-    """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+    """From challenge 01:
+    Calc a given word value based on Scrabble LETTER_SCORES mapping"""
     return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
 
 
-# re-use from challenge 01
 def max_word_value(words):
-    """Calc the max value of a collection of words"""
+    """From challenge 01:
+    Calc the max value of a collection of words"""
     return max(words, key=lambda w: calc_word_value(w))
 
 
 def draw_letters():
-    """Draw a random set of NUM_LETTERS letters. Hint: use random"""
+    """Pick NUM_LETTERS letters randomly. Hint: use stdlib random"""
     pass
 
 
 def input_word(draw):
-    """Ask user to input a word. Check vs. draw letters if user is using right letters.
-    Also check if the given word is in the DICTIONARY set"""
+    """Ask player for a word. Validations: 1) only use letters of draw, 2) valid dictionary word"""
     pass
 
 
-def get_valid_dict_words(draw):
-    """Take the draw letters, calculate all possible words, then cross-check with DICTIONARY (= set).
-    Return the result. Hint: use itertools.permutations and set operations."""
+def get_possible_dict_words(draw):
+    """Generate a collection of all possible words in DICTIONARY (set) from draw of NUM_LETTERS
+    Hint: use itertools.permutations"""
     pass
 
 
@@ -45,18 +44,17 @@ def main():
 
     word = input_word(draw)
     word_score = calc_word_value(word)
-    print('Word chosen: {} with value: {}'.format(word, word_score))
+    print('Word chosen: {} (value: {})'.format(word, word_score))
 
-    possible_words = get_valid_dict_words(draw)
+    possible_words = get_possible_dict_words(draw)
 
     max_word = max_word_value(possible_words)
     max_word_score = calc_word_value(max_word)
-    print('Max word: {} with value: {}'.format(max_word, max_word_score))
+    print('Optimal word possible: {} (value: {})'.format(max_word, max_word_score))
 
-    game_score = float(word_score) / max_word_score * 100
-    print('You scored: {}'.format(game_score))
-    
-    # bonus: make scores persistent and delight user when he/she breaks a new record
+    game_score = word_score / max_word_score * 100
+    print('You scored: {:.1f}'.format(game_score))
+
 
 if __name__ == "__main__":
     main()
