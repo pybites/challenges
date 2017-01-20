f76b67b1a7114083ee75f64262166b7155a9bfa9
pybites <pybites@projects.bobbelderbos.com>
2017-01-20 10:56:25 +0100

    added solution for 02

---
+++ 02/game.py
@@ -0,0 +1,93 @@
+#!python3
+# Code Challenge 02 - Word Values Part II - a simple game
+# http://pybit.es/codechallenge02.html
+
+import itertools
+import random
+
+from data import DICTIONARY, LETTER_SCORES, POUCH
+
+NUM_LETTERS = 7
+
+
+class WrongInput(ValueError):
+    pass
+
+
+def draw_letters():
+    """Pick NUM_LETTERS letters randomly. Hint: use stdlib random"""
+    return random.sample(POUCH, NUM_LETTERS)
+
+
+def input_word(draw):
+    """Ask player for a word.
+    Validations: 1) only use letters of draw, 2) valid dictionary word"""
+    while True:
+        word = input('Form a valid word: ').upper()
+        try:
+            return _validation(word, draw)
+        except WrongInput as exc:
+            print(exc)
+            continue
+
+
+def _validation(word, draw):
+    if not set(word) < set(draw):
+        raise WrongInput('One or more characters not in draw, try again')
+    if not word.lower() in DICTIONARY:
+        raise WrongInput('Not a valid dictionary word, try again')
+    return word
+
+
+# From challenge 01:
+def calc_word_value(word):
+    """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+
+
+# Below 2 functions pass through the same 'draw' argument (smell?).
+# Maybe you want to abstract this into a class?
+# get_possible_dict_words and _get_permutations_draw would be instance methods.
+# 'draw' would be set in the class constructor (__init__).
+def get_possible_dict_words(draw):
+    """Get all possible words from draw which are valid dictionary words.
+    Use the _get_permutations_draw helper and DICTIONARY constant"""
+    permutations = [''.join(word).lower() for word in _get_permutations_draw(draw)]
+    return set(permutations) & set(DICTIONARY)
+
+
+def _get_permutations_draw(draw):
+    """Helper for get_possible_dict_words to get all permutations of draw letters.
+    Hint: use itertools.permutations"""
+    for i in range(1, 8):
+        yield from list(itertools.permutations(draw, i))
+
+
+# From challenge 01:
+def max_word_value(words):
+    """Calc the max value of a collection of words"""
+    return max(words, key=calc_word_value)
+
+
+def main():
+    """Main game interface calling the previously defined methods"""
+    draw = draw_letters()
+    print('Letters drawn: {}'.format(', '.join(draw)))
+
+    word = input_word(draw)
+    word_score = calc_word_value(word)
+    print('Word chosen: {} (value: {})'.format(word, word_score))
+
+    possible_words = get_possible_dict_words(draw)
+
+    max_word = max_word_value(possible_words)
+    max_word_score = calc_word_value(max_word)
+    print('Optimal word possible: {} (value: {})'.format(
+        max_word, max_word_score))
+
+    game_score = word_score / max_word_score * 100
+    print('You scored: {:.1f}'.format(game_score))
+
+
+if __name__ == "__main__":
+    main()
