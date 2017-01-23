927e52d18b0c639097ca3fc3377964cb03463d22
pybites <pybites@projects.bobbelderbos.com>
2017-01-23 01:45:35 +0100

    possible solution

--- 03/tags.py
+++ 03/tags.py
@@ -1,38 +1,49 @@
 from collections import Counter
+from difflib import SequenceMatcher
+from itertools import product
 import re
 
-from test import parse_tags_html
-
-FROM_TO = str.maketrans('-', ' ')
-MSG = '{}, {} not in test set'
-NUM_ITEMS = 10
-RSS_FEED = 'all.rss.xml'
-TAG = re.compile(r'<category>([^<]+)</category>')
-
-
-def _normalize(words):
-    """Make words lowercase and replace characters"""
-    return [w.translate(FROM_TO) for w in words]
+REPLACE_CHARS = str.maketrans('-', ' ')
+IDENTICAL = 1.0
+TOP_NUMBER = 10
+RSS_FEED = 'rss.xml'
+SIMILAR = 0.87
+TAG_HTML = re.compile(r'<category>([^<]+)</category>')
 
 
-def _read_rss():
-    """Reads the RSS feed in"""
+def get_tags():
+    """Find all tags (TAG_HTML) in RSS_FEED.
+    Replace dash with whitespace (REPLACE_CHARS)"""
     with open(RSS_FEED) as f:
-        return f.read().lower()
+        tags = TAG_HTML.findall(f.read().lower())
+    return [tag.translate(REPLACE_CHARS) for tag in tags]
 
 
-def count_tags():
-    """Count the number of tags in our RSS feed"""
-    xml = _read_rss()
-    tags = TAG.findall(xml)
-    tags_normalized = _normalize(tags)
-    counter = Counter(tags_normalized)
-    return(counter.most_common(NUM_ITEMS))
+def get_top_tags(tags):
+    """Get the TOP_NUMBER of most common tags"""
+    return Counter(tags).most_common(TOP_NUMBER)
+
+
+def get_similarities(tags):
+    """Find set of tags pairs with similarity ratio of > SIMILAR"""
+    for pair in product(tags, tags):
+        # performance enhancements 1.992s -> 0.144s
+        if pair[0][0] != pair[1][0]:
+            continue
+        pair = tuple(sorted(pair))  # set needs hashable type
+        similarity = SequenceMatcher(None, *pair).ratio()
+        if SIMILAR < similarity < IDENTICAL:
+            yield pair
 
 
 if __name__ == "__main__":
-    tests = list(parse_tags_html())
-    counts = count_tags()
-    for tag, count in counts:
-        assert (tag, count) in tests, MSG.format(tag, count)
-        print('{:<20} {}'.format(tag,count))
+    tags = get_tags()
+    top_tags = get_top_tags(tags)
+    print('* Top {} tags:'.format(TOP_NUMBER))
+    for tag, count in top_tags:
+        print('{:<20} {}'.format(tag, count))
+    similar_tags = dict(get_similarities(tags))
+    print()
+    print('* Similar tags:')
+    for singular, plural in similar_tags.items():
+        print('{:<20} {}'.format(singular, plural))
