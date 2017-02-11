a4a0acb7979624155795a7ca660d04b6ebe2a30c
pybites <pybites@projects.bobbelderbos.com>
2017-02-11 01:44:02 +0100

    tweaks, comply flake8

--- 05/similar_tweeters.py
+++ 05/similar_tweeters.py
@@ -1,24 +1,30 @@
-from collections import Counter
 import csv
 import glob
-import itertools
 import os
 import re
 from string import ascii_lowercase
 import sys
 
-from gensim import corpora, models, matutils, similarities
+from gensim import corpora, models, similarities
 from nltk.corpus import stopwords
 
 CSV = 'data/{}.csv'
 DB = 'twitter.dict'
 IS_LINK_OBJ = re.compile(r'^(?:@|https?://)')
 STOPWORDS = set(stopwords.words('english'))
-IS_ASCII = lambda w: all(ord(c) < 128 for c in w)
-STRIP_NON_ASCII = lambda w: ''.join([i for i in w if i in ascii_lowercase])
 
-def similar_tweeters(*users):
-    pass
+
+def _is_ascii(w):
+    return all(ord(c) < 128 for c in w)
+
+
+def _strip_non_ascii(w):
+    return ''.join([i for i in w if i in ascii_lowercase])
+
+
+def _get_filename(u):
+    return os.path.splitext(os.path.basename(u))[0]
+
 
 def get_user_tokens(user):
     tweets_csv = CSV.format(user)
@@ -29,13 +35,14 @@ def get_user_tokens(user):
             for w in row['text'].lower().split():
                 words.append(w)
     return tokenize_text(words)
-   
+
+
 def tokenize_text(words):
     words = [word for word in words if len(word) > 4]
-    words = [word for word in words if IS_ASCII(word)]
+    words = [word for word in words if _is_ascii(word)]
     words = [word for word in words if not IS_LINK_OBJ.search(word)]
     words = [word for word in words if word not in STOPWORDS]
-    words = [STRIP_NON_ASCII(word) for word in words]
+    words = [_strip_non_ascii(word) for word in words]
     return words
 
 
@@ -47,17 +54,18 @@ if __name__ == "__main__":
         user = 'bbelderbos'
     if len(sys.argv) > 2:
         diff_users = sys.argv[1:]
-    else: 
-        diff_users = [i for i in glob.glob(CSV.format('*')) if not user in i]
-        diff_users = [os.path.splitext(os.path.basename(u))[0] for u in diff_users]
-    
+    else:
+        diff_users = [i for i in glob.glob(CSV.format('*')) if user not in i]
+        diff_users = [_get_filename(u) for u in diff_users]
+
     data = []
     for du in diff_users:
         data.append(get_user_tokens(du))
     dictionary = corpora.Dictionary(data)
-    
+
     corpus = [dictionary.doc2bow(text) for text in data]
-    lda = models.ldamodel.LdaModel(corpus, num_topics=5, id2word=dictionary, passes=15)
+    lda = models.ldamodel.LdaModel(corpus, num_topics=5,
+                                   id2word=dictionary, passes=15)
 
     index = similarities.MatrixSimilarity(lda[corpus])
 
