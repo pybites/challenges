11f1e22f9029f5dc756f1ba787d050bc99284933
jmtaysom <james.taysom@gmail.com>
2017-03-07 18:42:52 -0500

    create a pony db and store the data

--- 08/inventory.py
+++ 08/inventory.py
@@ -1,5 +1,46 @@
-import sys
-print(sys.version)
+from pony.orm import *
+
+db = Database()
+
+class Room(db.Entity):
+    id = PrimaryKey(int, auto=True)
+    name = Required(str)
+    items = Required(Json)
+
+db.bind('sqlite', ':memory:', create_db=True)
+db.generate_mapping(create_tables=True)
+
+@db_session
+def create_inventory():
+    Living = Room(name = 'Living Room',
+              items = {
+                'TV':300,
+                'Couch':500,
+                'sideboard':400
+              })
+
+    Dining = Room(name = 'Dining Room',
+              items = {
+                'Table': 500,
+                'Fish Tank': 100,
+                'Painting': 500
+              })
+
+    Kitchen = Room(name = 'Kitchen',
+                   items = {
+                       'Fridge': 500,
+                       'Stove': 300,
+                       'Mixer': 300
+                   })
+
+    Bedroom = Room(name = 'Bedroom',
+                   items = {
+                       'bed': 1000,
+                       'desk': 100,
+                       'shelves': 100
+                   })
+
+create_inventory()
 
 inventory  = {'living room': {'TV':300, 'Couch':500,'sideboard':400},
               'dining roon': {'Table': 500, 'Fish Tank': 100, 'Painting': 500},
