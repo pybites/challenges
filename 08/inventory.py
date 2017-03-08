dbd0893badc026db0906bb417f73745cff4f9e39
jmtaysom <james.taysom@gmail.com>
2017-03-08 18:52:45 -0500

    switched from pony to mongodb

--- 08/inventory.py
+++ 08/inventory.py
@@ -1,53 +1,41 @@
-from pony.orm import *
+import pymongo
+client = pymongo.MongoClient()
+db = client.inventory
+house = db.house
 
-db = Database()
-
-class Room(db.Entity):
-    id = PrimaryKey(int, auto=True)
-    name = Required(str)
-    items = Required(Json)
-
-db.bind('sqlite', ':memory:', create_db=True)
-db.generate_mapping(create_tables=True)
-
-@db_session
-def create_inventory():
-    Living = Room(name = 'Living Room',
-              items = {
+def create_inventory(collection):
+    Rooms=[{'name' : 'Living Room',
+             'items' : {
                 'TV':300,
                 'Couch':500,
                 'sideboard':400
-              })
-
-    Dining = Room(name = 'Dining Room',
-              items = {
+            }},
+            {'name' : 'Dining Room',
+             'items' : {
                 'Table': 500,
                 'Fish Tank': 100,
                 'Painting': 500
-              })
+            }},
+            {'name' : 'Kitchen',
+             'items' : {
+                'Fridge': 500,
+                'Stove': 300,
+                'Mixer': 300
+            }},
+            {'name' : 'Bedroom',
+             'items' : {
+                'bed': 1000,
+                'desk': 100,
+                'shelves': 100
+            }}
+    ]
+    collection.insert_many(Rooms)
 
-    Kitchen = Room(name = 'Kitchen',
-                   items = {
-                       'Fridge': 500,
-                       'Stove': 300,
-                       'Mixer': 300
-                   })
+if house.count() == 0:
+    create_inventory(house)
 
-    Bedroom = Room(name = 'Bedroom',
-                   items = {
-                       'bed': 1000,
-                       'desk': 100,
-                       'shelves': 100
-                   })
 
-create_inventory()
-
-inventory  = {'living room': {'TV':300, 'Couch':500,'sideboard':400},
-              'dining roon': {'Table': 500, 'Fish Tank': 100, 'Painting': 500},
-              'kitchen': {'Fridge': 500, 'Stove': 300, 'Mixer': 300},
-              'Bedroom': {'bed': 1000, 'desk': 100, 'shelves': 100}}
-
-for room, items in inventory.items():
-    print(f'In the {room} there are the following items: ')
-    for item, value in items.items():
+for room in house.find():
+    print(f'In the {room["name"]} there are the following items: ')
+    for item, value in room["items"].items():
         print(f'    A {item} worth ${value}')
