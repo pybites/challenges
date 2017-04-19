f60bf7b0a369f26a01bbf6f4d2fcad034fdf517d
Martin Uribe <clamytoe@gmail.com>
2017-04-19 13:55:04 -0500

    Added comments and cleaned up the code.
    Signed-off-by: Martin Uribe <clamytoe@gmail.com>

--- 15/app-clamytoe.py
+++ 15/app-clamytoe.py
@@ -1,7 +1,6 @@
 from flask import Flask, request
 from flask import render_template
 from flask import redirect
-from datetime import datetime, date
 from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -11,6 +10,7 @@ db = SQLAlchemy(app)
 
 
 class Tasks(db.Model):
+    """Database schema"""
     task_id = db.Column(db.Integer, primary_key=True)
     task = db.Column(db.Text)
     status = db.Column(db.Boolean, default=False)
@@ -23,23 +23,32 @@ class Tasks(db.Model):
         return '<Task %s>' % self.task
 
 
+# initialize the database
 db.create_all()
 
 
 @app.route('/')
 def index():
+    """Home page of the app
+    
+    It loads the template page and passes on any current tasks that exist.
+    """
     tasks = Tasks.query.all()
     return render_template('clamytoe.html', tasks=tasks)
 
 
 @app.route('/add', methods=['POST'])
 def add_task():
+    """Adds a new task
+    
+    Redirexts to home page is there is no task.
+    """
     task = request.form['task']
     
     if not task:
         return redirect('/')
 
-    status = int(request.form['status'])
+    status = bool(int(request.form['status']))
 
     new_task = Tasks(task, status)
     db.session.add(new_task)
@@ -49,6 +58,10 @@ def add_task():
 
 @app.route('/delete/<int:task_id>')
 def delete_task(task_id):
+    """Deletes task by its ID
+    
+    If the task does not exist, redirects to home page.
+    """
     task = Tasks.query.get(task_id)
 
     if not task:
@@ -59,15 +72,13 @@ def delete_task(task_id):
     return redirect('/')
 
 
-@app.route('/clear')
-def clear_all():
-    db.drop_all()
-    db.create_all()
-    return redirect('/')
-
-
 @app.route('/close/<int:task_id>')
 def close_task(task_id):
+    """Changes the state of a task
+    
+    If the task is open, it closes it. If it's close, it opens it.
+    Redirects to home page if the task does not exists.
+    """
     task = Tasks.query.get(task_id)
 
     if not task:
@@ -79,6 +90,17 @@ def close_task(task_id):
         task.status = True
 
     db.session.commit()
+    return redirect('/')
+
+
+@app.route('/clear')
+def clear_all():
+    """Dumps all tasks from the database
+     
+     It then re-initializes the database for continued use.
+     """
+    db.drop_all()
+    db.create_all()
     return redirect('/')
 
 
