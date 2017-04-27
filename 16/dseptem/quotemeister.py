e38f122c7ba8830605566ecd1e42a3c02725cf89
dseptem <d.septem@gmail.com>
2017-04-27 02:57:27 -0300

    +Keep It Simple, Stupid applied +only 3 tries to query for quote +refactor

--- 16/dseptem/quotemeister.py
+++ 16/dseptem/quotemeister.py
@@ -1,9 +1,7 @@
 import webbrowser
 from json import JSONDecodeError
-from multiprocessing import Process
 from flask import Flask, render_template, abort
 import requests
-import webview
 
 app = Flask(__name__)
 
@@ -19,47 +17,29 @@ def wisdom():
 
 
 def get_random_quote():
-    errors = 0
-    while True:
-        if errors > 2:
-            abort(500)
+    tries = 0
+    while tries < 2:
         try:
             j = requests.post(quotes_url, data={'method': 'getQuote', 'format': 'json', 'lang': 'en'}).json()
             return j['quoteText'], j['quoteAuthor']
         except JSONDecodeError:
-            errors += 1
-            continue
+            tries += 1
+    else:
+        abort(500)
 
 
 def get_author_bio(author_name):
     query_url = wiki_url.format(title=author_name)
+    not_available = "Biography not available =("
     try:
         j = requests.get(query_url).json()
-        for p in j['query']['pages']:
-            if 'may refer to' in j['query']['pages'][p]['extract']:
-                return "Biography not available =("
-            return j['query']['pages'][p]['extract']
-    except:
-        return "Biography not available =("
+        pages = j['query']['pages']
+        for page in pages:
+            return pages[page]['extract'] if 'may refer to' not in pages[page]['extract'] else not_available
+    except (KeyError, JSONDecodeError):
+        return not_available
 
-
-def start_web():
-    app.run()
-
-
-def start_view():
-    webview.create_window('Wisdom of the Ages', 'http://127.0.0.1:5000')
 
 if __name__ == "__main__":
-    command = input('(S)imple view or (W)eb view?')
-    while command.lower() not in ['s', 'w']:
-        input('(S)imple view or (W)eb view?')
-    server = Process(target=start_web)
-    server.start()
-    if command == 's':
-        view = Process(target=start_view)
-        view.start()
-        view.join()
-        server.terminate()
-    else:
-        webbrowser.open('http://127.0.0.1:5000')
+    webbrowser.open('http://127.0.0.1:5000')
+    app.run()
