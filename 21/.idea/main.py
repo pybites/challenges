4cbf494df1d508b579fa7383a487ee5e93d578b5
Norifusa Tachiyanagi <wonderfulboyx@gmail.com>
2017-05-31 03:21:17 +0900

    add django project to 21

--- 21/.idea/main.py
+++
@@ -1,8 +0,0 @@
-#@param comsumption wattage consumption of a device [W]
-#@param cost cost per kilowatt hour
-#@param hour time [h]
-#
-#@return total_cost [kWh]
-def CostCulculation(comsumption, cost, hour=1):
-    return cost*(comsumption/1000.0*hour)
-    
