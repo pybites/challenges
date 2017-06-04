a4196b1d2ad63e5110ad60491f2d33aa3a5f516e
bbelderbos <bobbelderbos@gmail.com>
2017-06-04 23:20:39 +0200

    moved data to own dir use downloaded power table instead of requests

--- 21/bbelderbos/data.py
+++ 21/bbelderbos/data.py
@@ -1,4 +1,5 @@
 import csv
+import os
 from pprint import pprint as pp
 import re
 
@@ -9,8 +10,10 @@ from bs4 import BeautifulSoup as soup
 # Electricity prices components for domestic consumers
 # filtered on 2016 / Band DC 2500-5000 kWh
 # http://ec.europa.eu/eurostat/web/energy/data/database
-PRICES_CSV = 'nrg_pc_204_c_1_Data.csv'
-POWER_TABLE = 'https://www.wholesalesolar.com/solar-information/how-to-save-energy/power-table'
+DATA_DIR = 'data'
+PRICES_CSV = os.path.join(DATA_DIR, 'nrg_pc_204_c_1_Data.csv')
+POWER_TABLE = os.path.join(DATA_DIR, 'power-table')
+POWER_TABLE_URL = 'https://www.wholesalesolar.com/solar-information/how-to-save-energy/power-table'
 
 
 def get_energy_prices():
@@ -19,26 +22,41 @@ def get_energy_prices():
             yield row['GEO'], row['Value']
 
 
+def _get_power_table():
+    try:
+        with open(POWER_TABLE) as f:
+            html = f.read()
+    except IOError:
+        print('no local power table, get it from the web')
+        r = requests.get(POWER_TABLE_URL)
+        html = r.text
+    s = soup(html, 'lxml')
+    return s.find('table', attrs={'class': 'powerTable'})
+
+
 def get_appliance_wattages():
-    r = requests.get(POWER_TABLE)
-    s = soup(r.text, 'lxml')
-    table = s.find('table', attrs={'class': 'powerTable'})
+    table = _get_power_table()
     rows = table.findAll('tr')
     for tr in rows:
         cols = tr.findAll('td')
         for td in cols:
+            # Flat is better than nested.
+            # still bit too long, need to refactor
+            # parsing html tables sucks
             try:
                 bgcolor = td["bgcolor"].lower()
             except KeyError:
-                continue 
+                continue
             if 'e0e0e0' not in bgcolor:
-                continue 
+                continue
             appliance = td.text.strip()
             if not appliance:
                 continue
             wattage = td.findNext('td')
             wattage = wattage.text.strip()
-            if not wattage or not re.search(r'^[0-9]', wattage) or '/day' in wattage:
+            if not wattage or not re.search(r'^[0-9]', wattage):
+                continue
+            if '/day' in wattage:
                 continue
             wattage = re.sub(r'([-,0-9]+).*', r'\1', wattage)
             wattage = wattage.replace(',', '')
