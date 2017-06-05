d805e2d24961d09f3dfd1702cfdc703407e41e23
Norifusa Tachiyanagi <wonderfulboyx@gmail.com>
2017-06-05 17:15:38 +0900

    commit

--- 21/wonderfulboyx/mysite/settings.py
+++ 21/wonderfulboyx/mysite/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/1.10/ref/settings/
 """
 
 import os
-
+from os import environ
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
@@ -20,10 +20,10 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/1.10/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'tod%r(01!)#pasu2wkn_xamk3z0r@nn!adp71uf3d7myqj_^^!'
+SECRET_KEY = environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['127.0.0.1', '.pythonanywhere.com']
 
