fd46c26d89b88596441d7770d03d26f5d587eae2
bbelderbos <bobbelderbos@gmail.com>
2017-07-09 17:57:49 +0200

    almost done: matplotlib stats, but getting _tkinter.TclError: image pyimage doesn't exist

--- 26/bbelderbos/pomodoro.py
+++ 26/bbelderbos/pomodoro.py
@@ -1,28 +1,35 @@
 from collections import Counter
-import datetime
+from datetime import datetime, timedelta
+from operator import itemgetter
+from random import randint
 import sqlite3
 import sys
 
 import easygui as g
+import matplotlib.pyplot as plt
+import numpy as np
 
 POMODORI_AMOUNTS = range(1, 6)
-OPTIONS = ['Log pomodori', 'Show stats', 'Exit']
+OPTIONS = ['Log pomodori', 'Show stats', 'Reset', 'Exit']
 POMODORO_IMG = 'pomodoro.gif'
 POMODORO_MIN = 25
-TOMATO_CHAR = u'\U0001F345'
-TOMATE_SAFE_CHAR = u'\U+1F345'
-print(TOMATE_SAFE_CHAR)
-sys.exit()
+PLOT_TITLE = 'How much am I reading each week?'
+STATS_IMG = 'stats.png'
+FAKE_DATA = True
 
 
 class PomodoriLogger:
 
-    def __init__(self):
+    def __init__(self, fake_data=FAKE_DATA):
         self.conn = sqlite3.connect('pomodoro.db',
                                     detect_types=sqlite3.PARSE_DECLTYPES)
         self.cursor = self.conn.cursor()
         self.cursor.execute('''CREATE TABLE IF NOT EXISTS logs
                                (date timestamp, pomodori integer)''')
+
+        if fake_data:
+            self._insert_fake_pomos()
+
         self.conn.commit()
 
     def __enter__(self):
@@ -30,6 +37,19 @@ class PomodoriLogger:
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.conn.close()
+
+    def _insert_fake_pomos(self):
+        self.cursor.execute('SELECT * FROM logs')
+        row = self.cursor.fetchone()
+        if row is not None:
+            return
+
+        for i in range(7, 36, 7):
+            date = datetime.now() - timedelta(days=i)
+            amount = randint(1, 10)
+            self.cursor.execute('INSERT INTO logs VALUES(?, ?)', (date,
+                                                                  amount))
+        self.conn.commit()
 
     def add_pomodori(self, amount=1):
         try:
@@ -40,8 +60,12 @@ class PomodoriLogger:
         if amount not in POMODORI_AMOUNTS:
             raise ValueError('Amount not in {}'.format(str(POMODORI_AMOUNTS)))
 
-        date = datetime.datetime.now()
+        date = datetime.now()
         self.cursor.execute('INSERT INTO logs VALUES(?, ?)', (date, amount))
+        self.conn.commit()
+
+    def reset(self):
+        self.cursor.execute('DELETE FROM logs')
         self.conn.commit()
 
     def _calc_stats(self):
@@ -56,31 +80,29 @@ class PomodoriLogger:
             stats[year_week] += amount
         return stats
 
+    def _make_graph(self, stats):
+        stats_sorted = sorted(stats.items(), key=itemgetter(0))
+
+        weeks = [p[0] for p in stats_sorted]
+        y_pos = np.arange(len(weeks))
+        pomos = [p[1] for p in stats_sorted]
+
+        plt.rcdefaults()
+        _, ax = plt.subplots()
+        ax.barh(y_pos, pomos, align='center',
+                color='red', ecolor='black')
+        ax.set_yticks(y_pos)
+        ax.set_yticklabels(weeks)
+        ax.invert_yaxis()  # labels read top-to-bottom
+        ax.set_xlabel('Pomodori')
+        ax.set_title(PLOT_TITLE)
+
+        plt.savefig(STATS_IMG)
+
     def show_stats(self):
         stats = self._calc_stats()
-        fmt = '{:<7} | {}'
-        divider = '-' * 80
-
-        header = fmt.format('Week', 'Pomodori')
-        print(header)
-        print(divider)
-
-        output = [header]
-        output.append(divider)
-
-        for week, num_pomos in stats.items():
-            pomo_symbol = TOMATO_CHAR + ' '
-            pomodori = num_pomos * pomo_symbol
-            line = fmt.format(week, pomodori)
-
-            # unicode does not work in tcl :(
-            # can print it to stdout though :)
-            print(line)
-            line = line.replace(TOMATO_CHAR, TOMATE_SAFE_CHAR)
-            output.append(line)
-
-        # text- and codebox give input field, I just want display only
-        g.buttonbox('\n'.join(output), image=POMODORO_IMG, choices=['Go back'])
+        self._make_graph(stats)
+        g.buttonbox('', image=STATS_IMG, choices=['Go back'])
 
 
 def main():
@@ -99,6 +121,12 @@ def main():
                 pomo.show_stats()
 
             elif action == 2:
+                msg = "Do you want to continue?"
+                title = "Please Confirm"
+                if g.ccbox(msg, title):
+                    pomo.reset()
+
+            elif action == 3:
                 print('Goodbye')
                 sys.exit(0)
 
