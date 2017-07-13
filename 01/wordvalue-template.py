ab0da5833803131a5ba788a21a2c7c673324b8a1
Khusid, Mikhail <KHUSIM1@nationwide.com>
2017-07-13 13:34:07 -0400

    27 dir and readme

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -2,13 +2,12 @@ from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
     """Load dictionary into a list and return list"""
-    with open(DICTIONARY) as f:
-        return [word.strip() for word in f.read().split()]
+    pass
 
 def calc_word_value():
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-
+    pass
 
 def max_word_value():
     """Calculate the word with the max value, can receive a list
