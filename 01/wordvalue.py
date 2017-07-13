d0c531bf27c10449f233c8c318c56038b68601be
Khusid, Mikhail <KHUSIM1@nationwide.com>
2017-07-13 13:33:49 -0400

    27 dir and readme

---
+++ 01/wordvalue.py
@@ -0,0 +1,21 @@
+from data import DICTIONARY, LETTER_SCORES
+
+def load_words():
+    """Load dictionary into a list and return list"""
+    with open(DICTIONARY) as f:
+        return [word.strip() for word in f.read().split()]
+
+def calc_word_value():
+    """Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES"""
+    
+
+def max_word_value():
+    """Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY"""
+    pass
+
+if __name__ == "__main__":
+    pass # run unittests to validate
+
+print(load_words())
