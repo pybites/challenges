354f0533ecef90aea0a92f432ca7d3b23a47f218
Khusid, Mikhail <KHUSIM1@nationwide.com>
2017-08-03 10:58:34 -0400

    8/1

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -9,17 +9,14 @@ def load_words():
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    for letter in word:
-        letter = letter.upper()
-        letter.replace('-', '')
-        sum()
+    return sum(LETTER_SCORES.get(letter.upper(), 0) for letter in word)
 
 
 def max_word_value(words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    return max(words or load_words(), key = lambda w: calc_word_value(w))
+
 
 if __name__ == "__main__":
-    print(calc_word_value('hello'))
-
+    print(max_word_value())
