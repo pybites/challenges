21f1dfac12780780fcb2bb8c16720d17e512ca34
pweids <paul.weidinger@gmail.com>
2017-10-28 09:24:50 -0400

    adding github API

--- 38/pweids/hacktoberfest_checker.py
+++ 38/pweids/hacktoberfest_checker.py
@@ -1,5 +1,6 @@
 from bottle import request, route, run, static_file
-import requests
+import json
+import urllib.request
 
 statements = [
     'It\'s not too late to start!',
@@ -11,14 +12,21 @@ statements = [
 ]
 
 def get_events(uname):
-	url = "https://api.github.com/users/{}/events".format(uname)
-	req = requests.get(url)
-	return r.json()
+	url = "https://api.github.com/search/issues?\
+q=-label:invalid+created:2017-09-30T00:00:00-12:00..2017-10-31T23:59:59-12:00\
++type:pr+is:public+author:{}".format(uname)
+	print(url)
+	req = urllib.request.Request(url)
+	with urllib.request.urlopen(req) as response:
+		result = json.loads(response.read().decode('utf-8'))
+	return len(result['items'])
 
 
 def message(uname):
-	json = get_events(uname)
-	
+	num_prs = get_events(uname)
+	index = min(num_prs, 5)
+	return "You have {} pull request(s). {}".format(index, statements[index])
+
 
 @route('/')
 def index():
