9c79740e2497f1be12832532fd7a5c1895a50841
Martin Uribe <clamytoe@users.noreply.github.com>
2017-11-03 08:34:55 -0500

    Modified file so that it would pass linting and so that it would run the tests.

--- 42/clamytoe/regex.py
+++ 42/clamytoe/regex.py
@@ -2,45 +2,51 @@ import re
 
 
 def extract_course_times():
-    '''Use re.findall to capture all mm:ss timestamps in a list'''
+    """Use re.findall to capture all mm:ss timestamps in a list"""
     flask_course = ('Introduction 1 Lecture 01:47'
                     'The Basics 4 Lectures 32:03'
                     'Getting Technical!  4 Lectures 41:51'
                     'Challenge 2 Lectures 27:48'
                     'Afterword 1 Lecture 05:02')
-    return re.findall(...
+    # return re.findall(...
+    pass
 
 
 def split_on_multiple_chars():
-    '''Use re.split to split log line by ; , .
+    """Use re.split to split log line by ; , .
        but not on the last ... so list should have len of 4
-       (hint check re.split docs for extra switches)'''
+       (hint check re.split docs for extra switches)"""
     logline = ('2017-11-03T01:00:02;challenge time,regex!.'
                'hope you join ... soon')
-    return re.split(...
+    # return re.split(...
+    pass
 
 
 def get_all_hashtags_and_links():
-    '''Use re.findall to extract the URL and 2 hashtags of this tweet'''
+    """Use re.findall to extract the URL and 2 hashtags of this tweet"""
     tweet = ('New PyBites article: Module of the Week - Requests-cache '
              'for Repeated API Calls - http://pybit.es/requests-cache.html '
              '#python #APIs')
-    return re.findall(...
+    # return re.findall(...
+    pass
 
 
 def match_first_paragraph():
-    '''Use re.sub to extract the content of the first paragraph (excl tags)'''
+    """Use re.sub to extract the content of the first paragraph (excl tags)"""
     html = ('<p>pybites != greedy</p>'
             '<p>not the same can be said REgarding ...</p>')
-    return re.sub(...
+    # return re.sub(...
+    pass
 
 
 def find_double_words():
-    '''Use re.search(regex, text).group() to find the double word'''
+    """Use re.search(regex, text).group() to find the double word"""
     text = 'Spain is so nice in the the spring'
-    return re.search(...
+    # return re.search(...
+    pass
 
 
 def match_ip_v4_address(ip):
-    '''Use re.match to match an ip v4 address (no need for exact IP ranges)'''
-    return re.match(...
+    """Use re.match to match an ip v4 address (no need for exact IP ranges)"""
+    # return re.match(...
+    pass
