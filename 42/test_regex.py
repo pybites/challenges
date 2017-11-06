ffe9a01562c47cbdf9a5d6bad65e2e8f36bf79b3
shalgrim <shalgrim@gmail.com>
2017-11-05 19:28:32 -0800

    clean bill of health

--- 42/test_regex.py
+++ 42/test_regex.py
@@ -1,32 +1,30 @@
-from regex import (extract_course_times, split_on_multiple_chars,
-                   get_all_hashtags_and_links, match_first_paragraph,
-                   find_double_words, match_ip_v4_address)
+import regex
 
 
 def test_extract_course_times():
     expected = ['01:47', '32:03', '41:51', '27:48', '05:02']
-    assert extract_course_times() == expected
+    assert regex.extract_course_times() == expected
 
 
 def test_split_on_multiple_chars():
     expected = ['2017-11-03T01:00:02', 'challenge time',
                 'regex!', 'hope you join ... soon']
-    assert split_on_multiple_chars() == expected
+    assert regex.split_on_multiple_chars() == expected
 
 
 def test_get_all_hashtags_and_links():
     expected = ['http://pybit.es/requests-cache.html', '#python', '#APIs']
-    assert get_all_hashtags_and_links() == expected
+    assert regex.get_all_hashtags_and_links() == expected
 
 
 def test_match_first_paragraph():
     expected = 'pybites != greedy'
-    assert match_first_paragraph() == expected
+    assert regex.match_first_paragraph() == expected
 
 
 def test_find_double_words():
     expected = 'the the'
-    assert find_double_words() == expected
+    assert regex.find_double_words() == expected
 
 
 def test_match_ip_address():
@@ -35,6 +33,6 @@ def test_match_ip_address():
                  '127.0.0.1']
     bad_ips = ['10.10.10', '10.10', '10', 'a.a.a.a', '10.0.0.a']
     for valid_ip in valid_ips:
-        assert match_ip_v4_address(valid_ip)
+        assert regex.match_ip_v4_address(valid_ip)
     for bad_ip in bad_ips:
-        assert match_ip_v4_address(bad_ip) is None
+        assert regex.match_ip_v4_address(bad_ip) is None
