db3b37b823b2007420ebcae02d04443787611fff
mridubhatnagar <mridubhatnagar@rediffmail.com>
2017-11-13 00:19:26 +0530

    Tag Analaysis Week-03 challenge Tag Analysis. Lists the top 10 tags based on the count of each tag. Gives the list of similar words. Visualization through Bar graphs using Ploty. Based on the tags and count.

--- 03/tags.py
+++ 03/tags.py
@@ -1,5 +1,10 @@
+from difflib import SequenceMatcher
+import plotly.plotly as py
+import plotly.graph_objs as go
+
 import feedparser
 import operator
+import itertools
 
 TOP_NUMBER = 10
 RSS_FEED = 'rss.xml'
@@ -7,7 +12,7 @@ SIMILAR = 0.87
 
 
 def get_tags():
-    """Find all tags in RSS_FEED.
+    """Find all tags in live feed.
     Replace dash with whitespace."""
     tags=[]
     blog_feed = feedparser.parse('https://pybit.es/feeds/all.rss.xml')
@@ -18,13 +23,11 @@ def get_tags():
     return tags
     
 
-        
-    
-    
-
-
 def get_top_tags(tags):
-    """Get the TOP_NUMBER of most common tags"""
+    """
+    Get the TOP_NUMBER of most common tags.
+    tags: List of all the tags used by the website.
+    """
     tag_list=[]
     D={}
     top_tags={}
@@ -37,8 +40,41 @@ def get_top_tags(tags):
 
 
 def get_similarities(tags):
-    """Find set of tags pairs with similarity ratio of > SIMILAR"""
-    pass
+    """
+    Find set of tags pairs with similarity ratio of > SIMILAR.
+    Argument:
+    tags: List of all the tags used by the website.
+
+    """
+    D={}
+    for word in tags:
+        word=word.replace(' ','').lower()
+        for words in tags:
+            words=words.replace(' ','').lower()
+            value = SequenceMatcher(None, word, words).ratio()
+            if SIMILAR<value<1:
+                D[word]=words
+    return D
+
+def visualizations(top_tags):
+    '''
+    Data visualization using Bar Graph.
+    Argument:
+    top_tags: List containing tuples.
+    And tuple have tag and count respectively.
+
+    x axis - tags
+    y axis - counts
+    '''
+    tags=[]
+    counts=[]
+    for tag,count in top_tags:
+        tags.append(tag)
+        counts.append(count)
+    data=[go.Bar(
+        x=tags,
+        y=counts)]
+    py.plot(data, filename='basic-visualization')
 
 
 if __name__ == "__main__":
@@ -47,11 +83,9 @@ if __name__ == "__main__":
     print('* Top {} tags:'.format(TOP_NUMBER))
     for tag, count in top_tags:
         print('{:<20} {}'.format(tag, count))
-    '''
-    similar_tags = dict(get_similarities(tags))
-    print()
-    
+    visualizations(top_tags)
+    similar_tags=get_similarities(tags)
     print('* Similar tags:')
     for singular, plural in similar_tags.items():
         print('{:<20} {}'.format(singular, plural))
-    '''
+
