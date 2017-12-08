8dbee6bbb156952c7d64387bf2ac71c1c9c4e5b6
Martin Uribe <clamytoe@users.noreply.github.com>
2017-12-08 10:46:24 -0600

    Wrote generic fizzbuzz function that would pass the test

---
+++ 45/clamytoe/fizzbuzz.py
@@ -0,0 +1,9 @@
+def fizzbuzz(num):
+    if num % 3 == 0 and num % 5 == 0:
+        return 'FizzBuzz'
+    elif num % 3 == 0:
+        return 'Fizz'
+    elif num % 5 == 0:
+        return 'Buzz'
+    else:
+        return num
