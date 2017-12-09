1994d11f1590f6a371faa0b0e81a1d2611013216
Martin Uribe <clamytoe@users.noreply.github.com>
2017-12-08 18:48:43 -0600

    Added more tests and added comments to the code

--- 45/clamytoe/fizzbuzz.py
+++ 45/clamytoe/fizzbuzz.py
@@ -1,5 +1,17 @@
 def check_value(value):
+    """
+    Validates the value that was passed into the program.
+
+    :param value: A positive integer is the only valid value
+    :return: int or an exception is raised
+    """
     if isinstance(value, bool):
+        raise ValueError
+    elif isinstance(value, str):
+        raise ValueError
+    elif isinstance(value, float):
+        raise ValueError
+    elif value <= 0:
         raise ValueError
     else:
         try:
@@ -12,17 +24,40 @@ def check_value(value):
 
 
 def fizz_check(num):
+    """
+    Determines the correct FiiBuzz value to return for the int given.
+
+    If the number is divisible by 3 'Fizz' is returned.
+    If the number is divisible by 5 'Buzz' is returned.
+    If the number is divisible by both 3 and 5, 'FizzBuzz' is returned.
+    If none of those are true, then number is just returned.
+
+    :param num: A positive integer
+    :return: 'Fizz', 'Buzz', 'FizzBuzz', or the num given
+    """
     value = check_value(num)
     return 'Fizz' * (value % 3 == 0) + 'Buzz' * (value % 5 == 0) or value
 
 
-def fizz_gen(limit=100):
+def fizz_gen(limit):
+    """
+    FizzBuzz generator.
+
+    :param limit: An integer representing the last number to be processed
+    :return: Integer or String returned after going through fizz_check()
+    """
     max_num = check_value(limit)
     for num in range(1, max_num + 1):
         yield fizz_check(num)
 
 
 def fizzbuzz(limit=100):
+    """
+    Super overcomplicated implementation of the FizzBuzz challenge.
+
+    :param limit: Default value of numbers to process
+    :return: Standard Out response after being processed
+    """
     value = check_value(limit)
     gen = fizz_gen(value)
     for _ in range(value):
--- 45/clamytoe/tests/test_fizzbuzz.py
+++ 45/clamytoe/tests/test_fizzbuzz.py
@@ -1,17 +1,36 @@
-from fizzbuzz import fizz_gen, fizz_check
+from fizzbuzz import check_value, fizz_gen, fizz_check
 from pytest import raises
 
 
-def test_fizz_check():
-    assert fizz_check(1) == 1
-    assert fizz_check(3) == 'Fizz'
-    assert fizz_check(5) == 'Buzz'
-    assert fizz_check(15) == 'FizzBuzz'
+def test_check_value():
+    """ Tests for invalid values """
+    with raises(ValueError):
+        check_value(True)
+    with raises(ValueError):
+        check_value(0)
+    with raises(ValueError):
+        check_value(-3)
+    with raises(ValueError):
+        fizz_check('15')
+    with raises(ValueError):
+        fizz_check(15.4563)
+    with raises(TypeError):
+        check_value(list())
     with raises(ValueError):
         fizz_check('Hello')
 
 
+def test_fizz_check():
+    """ Tests for valid values """
+    assert fizz_check(1) == 1
+    assert fizz_check(3) == 'Fizz'
+    assert fizz_check(5) == 'Buzz'
+    assert fizz_check(15) == 'FizzBuzz'
+    assert fizz_check(1287341875) == 'Buzz'
+
+
 def test_fizz_gen():
+    """ Tests for valid generator behavior """
     numbers = fizz_gen(2)
     assert next(numbers) == 1
     assert next(numbers) == 2
