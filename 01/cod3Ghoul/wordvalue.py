a396d18a3c838a4e096afc1ec5269ee752b91f01
Rene Sanchez <codeghoul.dev@gmail.com>
2017-12-23 21:24:56 -0600

    Add my PCC01 solution
    I had already been working on this solution, but I was working and committing off my origin/master branch and not the community branch. So, I am moving my soltuion to the community branch.

--- 01/cod3Ghoul/wordvalue.py
+++ 01/cod3Ghoul/wordvalue.py
@@ -1,18 +1,25 @@
 from data import DICTIONARY, LETTER_SCORES
 
+
 def load_words():
     """Load dictionary into a list and return list"""
-    pass
+    with open(DICTIONARY) as f:
+        return [word.strip() for word in f.read().split()]
+    
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
 
-def max_word_value():
+
+def max_word_value(word=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    if word is None:
+        word = load_words()
+    return max(word, key=calc_word_value)
+
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    pass
