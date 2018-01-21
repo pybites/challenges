c2dfdf7f3c41aa005b072c3c3ee1e732cc0ce628
Jason Castillo <jcastle13@jasons-mbp.attlocal.net>
2018-01-20 21:18:50 -0600

    Initial PCC05 file checked in.

---
+++ 05/jcastle13/similar_tweeters.py
@@ -0,0 +1,12 @@
+import sys
+
+def similar_tweeters(user1, user2):
+    pass
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print('Usage: {} <user1> <user2>'.format(sys.argv[0]))
+        sys.exit(1)
+
+    user1, user2 = sys.argv[1:3]
+    similar_tweeters(user1, user2)
