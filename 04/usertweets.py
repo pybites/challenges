04c8e10621c825c33b1ea47de01e0877f3546dcb
Alexander Jaworowski <alexander@jaworowski.se>
2018-02-11 15:31:07 +0100

    Improved test with mock of tweepy.API.user_timeline

--- 04/usertweets.py
+++ 04/usertweets.py
@@ -34,19 +34,19 @@ class UserTweets(object):
     def __init__(self, handle, max_id=None):
         self.id = handle
         self.output_file = os.path.join(DEST_DIR, handle + '.' + EXT)
-        self.tweets = self.tweets_list(max_id)
+        self.tweets = [Tweet(tw.id_str, tw.created_at, tw.text) for tw in self._get_tweets(max_id)]
         self._2csv()
 
-    def tweets_list(self, max_id):
-        return [Tweet(tw.id_str, tw.created_at, tw.text) for tw in self._get_tweets(max_id)]
-
     def _get_tweets(self, max_id, n=NUM_TWEETS):
+        n = n + 1  # Seems to be a bug in the api when calling tweepy.API.user_timeline() with cont parameter
         data = []
         try:
             if max_id:
-                data = tweepy.Cursor(self.api.user_timeline, id=self.id, max_id=max_id).items(limit=n)
+                #data = tweepy.Cursor(self.api.user_timeline, id=self.id, max_id=max_id).items(limit=n)
+                data = self.api.user_timeline(id=self.id, max_id=max_id, count=n)
             else:
-                data = tweepy.Cursor(self.api.user_timeline, id=self.id).items(limit=n)
+                #data = tweepy.Cursor(self.api.user_timeline, id=self.id).items(limit=n)
+                data = self.api.user_timeline(id=self.id, count=n)
         except Exception as e:
             print('Failed with Cursor: {}'.format(e))
         return data
@@ -69,14 +69,14 @@ class UserTweets(object):
 
 
 if __name__ == "__main__":
-
-    for handle in ('pybites', 'techmoneykids', 'bbelderbos'):
+    handles = ('pybites', 'techmoneykids', 'bbelderbos')
+    for handle in handles:
         print('--- {} ---'.format(handle))
         try:
-            user = UserTweets(handle)
+            user = UserTweets(handle, max_id='819831370113351680')
         except Exception as e:
             print('Failed: {}'.format(e))
         else:
-            for tw in user[:5]:
-                print(tw)
+            for tw in user:
+                print('{},'.format(tw))
         print()
