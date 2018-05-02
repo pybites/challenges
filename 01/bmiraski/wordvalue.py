bf3bd5d36020f49d621cf7e07b80fc4624afab18
Ben Miraski <bmiraski@bmiraski-m1.local>
2018-05-02 11:09:22 -0500

    PCC01 bmiraski

--- 01/bmiraski/wordvalue.py
+++ 01/bmiraski/wordvalue.py
@@ -1,19 +1,22 @@
-from data import DICTIONARY, LETTER_SCORES
-from typing import List
+"""Calculate word values from dictionary."""
+
+from data import DICTIONARY
+from data import LETTER_SCORES
 
 
 def load_words():
-    """Load dictionary into a list and return list"""
+    """Load dictionary into a list and return list."""
     with open(DICTIONARY) as g:
         words = g.read().split()
     return words
 
 
 def calc_word_value(word):
-    """Calculate the value of the word entered into function
-    using imported constant mapping LETTER_SCORES
-    """
+    """Return scrabble word value.
 
+    Calculate the value of the word entered into function
+    using imported constant mapping LETTER_SCORES.
+    """
     score = 0
     word = word.upper()
     for c in word:
@@ -26,8 +29,11 @@ def calc_word_value(word):
 
 
 def max_word_value(wordlist=load_words()):
-    """Calculate the word with the max value, can receive a list
-    of words as arg, if none provided uses default DICTIONARY"""
+    """Return the maximum word value from list.
+
+    Calculate the word with the max value, can receive a list
+    of words as arg, if none provided uses default DICTIONARY
+    """
     max_score = 0
     max_word = ""
     for word in wordlist:
@@ -44,14 +50,15 @@ if __name__ == "__main__":
     print(words[len(words)-5:len(words)])
     print(LETTER_SCORES)
     print(calc_word_value('zyzzogeton'))
-    print("The best scoring word is {} with a score of {}" \
-        .format(max_word_value(words), calc_word_value(max_word_value(words))))
-    print("The best scoring word is {} with a score of {}" \
-        .format(max_word_value(), calc_word_value(max_word_value())))
+    print("The best scoring word is {} with a score of {}"
+          .format(max_word_value(words),
+                  calc_word_value(max_word_value(words))))
+    print("The best scoring word is {} with a score of {}"
+          .format(max_word_value(), calc_word_value(max_word_value())))
     words_two = ['Alpha', 'Beta', 'Gamma']
-    print("The best scoring word is {} with a score of {}" \
-        .format(
-            max_word_value(words_two),
-            calc_word_value(max_word_value(words_two))
-            ))
+    print("The best scoring word is {} with a score of {}"
+          .format(
+              max_word_value(words_two),
+              calc_word_value(max_word_value(words_two))
+              ))
     # run unittests to validate
