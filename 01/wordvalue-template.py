9ef988f3d51e512a98ee6d92d1ad00e690bd263c
Artemsav <31589472+Artemsav@users.noreply.github.com>
2018-08-30 22:46:19 +0300

    load_words is working now, calc_word_value - i am looking for a way to use LETTER_SCORES

--- 01/wordvalue-template.py
+++ 01/wordvalue-template.py
@@ -1,14 +1,23 @@
 from data import DICTIONARY, LETTER_SCORES
 
 def load_words():
-    """Load dictionary into a list and return list"""
-    pass
+    with open('dictionary.txt', 'r') as dictionary:
+        list_data = []
+        data = dictionary.read()
+        elsp = data.split()
+        list_data.append(elsp)
+    return list_data
 
 def calc_word_value():
+    temp = ['doss', 'dbos', 'current', 'mother', 'father']
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    for i in temp:
+        join_word = ' '.join(i)
+        join_word.split(' ')
+        for k in join_word:
 
+    return LETTER_SCORE
 def max_word_value():
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
@@ -16,3 +25,6 @@ def max_word_value():
 
 if __name__ == "__main__":
     pass # run unittests to validate
+
+print(load_words())
+print(calc_word_value())
