55a14daea5e61b039a1d0997c2baf30cf2af5a15
Miguel Garcia Gil <sorian@gmail.com>
2018-10-02 21:27:46 +0200

    Second version

--- 01/sorian/wordvalue.py
+++ 01/sorian/wordvalue.py
@@ -11,28 +11,15 @@ def load_words():
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    value = 0
-    for char in word:
-        char = char.upper()
-        if char in LETTER_SCORES:
-            value += LETTER_SCORES[char]
-    return value
+    return sum([LETTER_SCORES.get(c.upper(),0) for c in word])
 
 def max_word_value(words=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    max_word = None
-    max_value = 0
-
     if not words:
         words = load_words()
 
-    for word in words:
-        value = calc_word_value(word)
-        if value > max_value:
-            max_word = word
-            max_value = value
-    return max_word
+    return max(words, key=calc_word_value)
 
 if __name__ == "__main__":
     pass # run unittests to validate
