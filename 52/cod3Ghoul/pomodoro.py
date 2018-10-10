b234c2cf831cdca8c51386dbe81ec9ce5b8603bf
cod3Ghoul <codeghoul.dev@gmail.com>
2018-10-10 17:21:29 +0000

    Add challenge requirements to pomodoro.py

--- 52/cod3Ghoul/pomodoro.py
+++ 52/cod3Ghoul/pomodoro.py
@@ -1,2 +1,8 @@
 """ PCC52 - Create your own Pomodoro Timer (24th of Sept 2018)
-    Instructions + submit work: https://codechalleng.es/challenges/52"""
+    Instructions + submit work: https://codechalleng.es/challenges/52
+    
+    The Challenge: 
+    - Create a timer for a set duration (eg 20 minutes) that "alarms" or notifies you at completion.
+    - Go a step further and allow the user to specify the amount of time the Pomodoro Timer goes for.
+    - Further develop the app by allowing it to loop. That is, Pomodoro Time > break time > Pomodoro Time > break time.
+    - Create a user interface if you have the time! PyGame or argparse perhaps? Maybe even make it web based with Flask or your other favourite web framework."""
