ab0278a85680cdfb89ebe9439aa351e1b94c182e
Martin Uribe <clamytoe@gmail.com>
2018-10-11 14:14:20 -0500

    PCC54 clamytoe

---
+++ 54/clamytoe/clip_mon.py
@@ -0,0 +1,39 @@
+from dataclasses import dataclass
+from datetime import datetime
+from hashlib import sha256
+from time import sleep
+
+from pyperclip import paste  # type: ignore
+
+clip_log: str = "clip.log"
+
+
+@dataclass
+class ClipMonitor:
+    last: str = ""
+
+    def __call__(self) -> None:
+        try:
+            while True:
+                if self.check_hash(paste()):
+                    self.save_it(paste())
+                sleep(5)
+        except KeyboardInterrupt:
+            print("\nAborted by user.")
+            exit()
+
+    def check_hash(self, content: str) -> bool:
+        previous: str = sha256(self.last.encode("utf-8")).hexdigest()
+        current: str = sha256(content.encode("utf-8")).hexdigest()
+        return False if previous == current else True
+
+    def save_it(self, content: str) -> None:
+        with open(clip_log, "a") as log:
+            now: datetime = datetime.now()
+            log.write(f"{now}\n--\n{content}\n--\n")
+            self.last = content
+
+
+if __name__ == "__main__":
+    clip_mon = ClipMonitor()
+    clip_mon()
