2f77528ea5dbb6248c47293aa4fae87a7ad4af35
cod3Ghoul <codeghoul.dev@gmail.com>
2018-10-11 19:07:09 +0000

    Refactor. Moved all print statements to their own methods.

--- 52/cod3Ghoul/pomodoro.py
+++ 52/cod3Ghoul/pomodoro.py
@@ -32,13 +32,12 @@ def pomodoro_timer(pomodoro_length=25):
         if minutes == pomodoro_length:
             pomodoros = pomodoros + 1
             os.system('clear')
-            print(f'You have completed {pomodoros} '
-                  f'{"pomodoro" if pomodoros == 1 else "pomodoros"}.')
+            display_pomodoros_completed()
             break
         os.system('clear')
         seconds = seconds + 1
-        print(f'Pomodoros Completed: {pomodoros}\n')
-        print(f'{minutes} {"Min" if minutes == 1 else "Mins"} {seconds} Secs')
+        display_pomodoros_completed()
+        display_timer(minutes, seconds)
         time.sleep(1)
 
 
@@ -56,13 +55,42 @@ def break_between_pomodoros(break_length=5):
             minutes = minutes + 1
         if minutes == break_length:
             os.system('clear')
-            print(f'{break_length} minute break is over!')
+            display_break(break_length, 'yes')
             break
         os.system('clear')
         seconds = seconds + 1
-        print(f'{break_length} Minute Break: '
-              f'{minutes} {"Min" if minutes == 1 else "Mins"} {seconds} Secs')
+        display_break(break_length)
+        display_timer(minutes, seconds)
         time.sleep(1)
+
+
+def display_timer(minutes, seconds):
+    """Display the active timer on screen.
+
+    Keyword arguments:
+    minutes -- minutes to display on screen when this method is called
+    seconds -- seconds to display on screen when this method is called
+    """
+    print(f'{minutes} {"Min" if minutes == 1 else "Mins"} {seconds} Secs')
+
+
+def display_pomodoros_completed():
+    """Display the total number of Pomodoros completed."""
+    global pomodoros
+    print(f'Pomodoros Completed: {pomodoros}\n')
+
+
+def display_break(break_length, break_over='no'):
+    """Display length of break or that the break is over.
+
+    Keyword arguments:
+    break_length -- length of break in minutes
+    break_over -- signals whether the break has ended
+                  using a 'yes' or 'no' string (default = 'no')"""
+    if break_over == 'yes':
+        print(f'{break_length} minute break is over!')
+    else:
+        print(f'{break_length} Minute Break: ')
 
 
 def main():
@@ -71,15 +99,15 @@ def main():
     if start.lower() == 's':
         while True:
             global pomodoros
-            pomodoro_timer()
+            pomodoro_timer(3)
             if pomodoros < 4:
                 short_break = input('Enter B to start a SHORT break: ')
                 if short_break.lower() == 'b':
-                    break_between_pomodoros()
+                    break_between_pomodoros(1)
             if pomodoros == 4:
                 long_break = input('Enter B to start a LONG break: ')
                 if long_break.lower() == 'b':
-                    break_between_pomodoros(20)
+                    break_between_pomodoros(2)
             con = input('\n1) Enter S to start your next pomodoro.\n'
                         '2) Enter R to reset your pomodoro count\n'
                         '   and start a new set of pomodoros.\n'
