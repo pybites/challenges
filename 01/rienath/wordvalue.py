44811fde5db031e64a51ca1a1e92c43168c14f08
rienath <infernalion@hotmail.com>
2018-10-15 00:07:07 +0100

    Add files via upload
    PCC01 rienath

---
+++ 01/rienath/wordvalue.py
@@ -0,0 +1,19 @@
+from data import DICTIONARY, LETTER_SCORES
+
+def load_words():
+    #Load dictionary into a list and return list
+    with open(DICTIONARY) as f:
+        return [word for word in f.read().split()]
+
+def calc_word_value(word):
+    #Calculate the value of the word entered into function using imported constant mapping LETTER_SCORES
+    return sum(LETTER_SCORES.get(letter.upper(), 0) for letter in word)
+
+def max_word_value(words=None):
+    #Calculate the word with the max value, can receive a list of words as arg, if none provided uses default DICTIONARY
+    if words is None:
+        words = load_words()
+    return max(words, key=calc_word_value)
+
+if __name__ == "__main__":
+    pass # run unittests to validate
