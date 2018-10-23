14ce1435d74600320ed502ea8c0cf29c9052495b
kincerb <kincerb@nationwide.com>
2018-10-22 21:55:20 -0400

    Template

---
+++ 13/kincerb/directors.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python3
+import collections
+import csv
+import os
+
+CSV_FILENAME = 'movie_metadata.csv'
+METADATA_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), CSV_FILENAME)
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
