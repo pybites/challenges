d536cc3ba229414de40319f7fb283c7be307365b
kincerb <kincerb@nationwide.com>
2018-10-23 23:05:36 -0400

    Challenge complete... :-/

--- 13/kincerb/directors.py
+++ 13/kincerb/directors.py
@@ -4,7 +4,7 @@ import csv
 import os
 
 CSV_FILENAME = 'movie_metadata.csv'
-METADATA_FILE = os.path.join(os.path.dirname(os.path.abspath(__file__)), CSV_FILENAME)
+METADATA_FILE = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file__))), CSV_FILENAME)
 MOVIE_MIN = 4
 YEAR_MIN = 1960
 TOP_DIRECTORS = 20
@@ -14,17 +14,19 @@ Movie = collections.namedtuple('Movie', 'title year score')
 def main():
     directors = collect_director_data()
     directors = collect_director_avg_scores(directors)
+    print_results(directors)
 
 
 def collect_director_avg_scores(directors):
     return {
         (director, _get_average(movies)): movies
         for director, movies in directors.items()
+        if len(movies) >= MOVIE_MIN
     }
 
 
 def _get_average(movies):
-    all_scores = [movie.imdb_score for movie in movies]
+    all_scores = [movie.score for movie in movies]
     return round(sum(all_scores) / max(1, len(all_scores)), 1)
 
 
@@ -34,7 +36,7 @@ def collect_director_data():
         try:
             director = row.get('director_name')
             movie = row.get('movie_title')
-            year = int(row.get('year', 0))
+            year = int(row.get('title_year', 0))
             score = float(row.get('imdb_score'))
         except ValueError:
             continue
@@ -42,7 +44,7 @@ def collect_director_data():
             continue
 
         directors[director].append(Movie(title=movie, year=year, score=score))
-    return _remove_movie_min_not_met(directors)
+    return directors
 
 
 def _remove_movie_min_not_met(directors):
@@ -57,6 +59,26 @@ def _row_in_csv(csv_file=METADATA_FILE):
         reader = csv.DictReader(f)
         for row in reader:
             yield row
+
+
+def print_results(directors):
+    fmt_director_entry = '{counter:>02}. {director:<52} {avg}'
+    fmt_movie_entry = '{year}] {title:<50} {score}'
+    sep_line = '-' * 60
+
+    for counter, (director_rating, movies) in \
+        enumerate(sorted(directors.items(), key=lambda x: float(x[0][1]),
+                  reverse=True)[:TOP_DIRECTORS], 1):
+        director, avg = director_rating
+
+        print()
+        print(fmt_director_entry.format(counter=counter,
+                                        director=director, avg=avg))
+        print(sep_line)
+
+        for m in sorted(movies, key=lambda m: m.score, reverse=True):
+            print(fmt_movie_entry.format(year=m.year,
+                                         title=m.title[:50], score=m.score))
 
 
 if __name__ == '__main__':
