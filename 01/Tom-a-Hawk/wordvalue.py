7a2a2e834a19670e281419052cfeac1d2204ff60
tommy <englerttw@mymail.vcu.edu>
2018-11-11 16:29:50 -0500

    PCC01 Tom-a-Hawk

--- 01/Tom-a-Hawk/wordvalue.py
+++ 01/Tom-a-Hawk/wordvalue.py
@@ -6,16 +6,30 @@ def load_words():
         x = fin.read().splitlines()
     return x
 
-def calc_word_value():
+def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-    pass
+    x = sum([LETTER_SCORES.get(char.upper(), 0) for char in word])
+    return x
 
-def max_word_value():
+def max_word_value(text=None):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    pass
+    if text is None:
+        x = load_words()
+        wordScores = [sum([LETTER_SCORES.get(char.upper(), 0) for char in i]) for i in x]
+        d = dict(zip(x, wordScores))
+        maximum = max(d, key=d.get)
+        return maximum
+    else:
+
+        wordScores = [sum([LETTER_SCORES.get(char.upper(), 0) for char in i]) for i in text]
+        d = dict(zip(text, wordScores))
+        maximum = max(d, key=d.get)
+        return maximum
 
 if __name__ == "__main__":
     load_words()
+    calc_word_value()
+    max_word_value()
     #pass # run unittests to validate
