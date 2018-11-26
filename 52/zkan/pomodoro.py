f18d216d96e6047ce232bcd8119f9c4682d8ec9e
zkan <kan@prontomarketing.com>
2018-11-26 21:19:44 +0700

    Create timer for 20 mins

---
+++ 52/zkan/pomodoro.py
@@ -0,0 +1,17 @@
+""" PCC52 - Create your own Pomodoro Timer (24th of Sept 2018)
+    Instructions + submit work: https://codechalleng.es/challenges/52"""
+
+from datetime import timedelta
+import time
+
+
+duration = 20 * 60
+while True:
+    print(timedelta(seconds=duration))
+    duration -= 1
+    if duration < 0:
+        break
+
+    time.sleep(1)
+
+print('Time\'s Up')
