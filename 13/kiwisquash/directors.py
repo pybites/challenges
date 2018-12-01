28252b4c73fab61121240b4bc4a3ef7d8f401e01
Ki Song <kiwisquash@gmail.com>
2018-11-30 20:03:24 -0800

    Director score displays 1 decimal place, the movies are listed in descnding scoring order.

--- 13/kiwisquash/directors.py
+++ 13/kiwisquash/directors.py
@@ -15,7 +15,18 @@ with open('../movie_metadata.csv',newline='') as csvfile:
                 directorData[director].append(movieInfo(title,year,score))
         except:
             pass
+
+def directorScore(director):
+    scores = []
+    for movie in directorData[director]:
+        scores.append(movie.score)
+    return sum(scores)/len(scores)
+
+def getKey(item):
+    return item.score 
+
 print(directorData)
 for director in directorData:
     if len(directorData[director])>3:
-        print(director,":",directorData[director][0].year)
+        # print(director,round(directorScore(director),1))
+        print(director,":",sorted(directorData[director],key = getKey, reverse = True))
