43a7e980c3a66f7dc263b9957c11d0d8a931a42e
Enric Joseph Ilagan <enricilagan@Enrics-MacBook-Pro.local>
2019-01-25 11:38:07 +0800

    PCC01 enricilagan

--- 01/enricilagan/word-value.py
+++ 01/enricilagan/word-value.py
@@ -1,4 +1,14 @@
-from data import DICTIONARY, LETTER_SCORES
+import os
+import urllib.request
+
+
+DICTIONARY = os.path.join('data/dictionary.txt')
+if not os.path.isfile(DICTIONARY):
+    urllib.request.urlretrieve('http://bit.ly/2iQ3dlZ', DICTIONARY)
+scrabble_scores = [(1, "E A O I N R T L S U"), (2, "D G"), (3, "B C M P"),
+                   (4, "F H V W Y"), (5, "K"), (8, "J X"), (10, "Q Z")]
+LETTER_SCORES = {letter: score for score, letters in scrabble_scores
+                 for letter in letters.split()}
 
 
 def load_words():
@@ -21,5 +31,10 @@ def max_word_value(words=None):
     return max(words, key=calc_word_value)
 
 
-if __name__ == "__main__":
-    max_word_value()
+def main():
+    print('Getting max word from dictionary.txt')
+    print(max_word_value(load_words()))
+
+
+if __name__ == '__main__':
+    main()
