82d89ea0697115493c3a2c0e4fa4092e2f41b9b7
Egan Done <edone@sympatico.ca>
2019-02-23 12:23:54 -0500

    Added some comments

--- 03/egandone/tags.py
+++ 03/egandone/tags.py
@@ -1,6 +1,5 @@
 from collections import Counter
 from difflib import SequenceMatcher
-from itertools import product
 import re
 
 IDENTICAL = 1.0
@@ -9,40 +8,41 @@ RSS_FEED = 'rss.xml'
 SIMILAR = 0.87
 TAG_HTML = re.compile(r'<category>([^<]+)</category>')
 
-
 def get_tags():
     """Find all tags (TAG_HTML) in RSS_FEED.
-    Replace dash with whitespace.
-    Hint: use TAG_HTML.findall"""
+    Replace dash with whitespace and make all tags lowercase.
+    """
     with open(RSS_FEED) as rss_feed:
         tags = [tag.replace('-', ' ').lower() for tag in TAG_HTML.findall(rss_feed.read())]
     return tags
 
 def get_top_tags(tags):
     """Get the TOP_NUMBER of most common tags
-    Hint: use most_common method of Counter (already imported)"""
+    """
     counter = Counter(tags)
     return counter.most_common(TOP_NUMBER)
 
-
 def get_similarities(tags):
     """Find set of tags pairs with similarity ratio of > SIMILAR
-    Hint 1: compare each tag, use for in for, or product from itertools (already imported)
-    Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
-    Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
+   """
     similiar_tags = [] 
+    # Use a set to get all the unique tags
     tag_set = set(tags)
+    # Copy of the original set of tags that is reduced by the current
+    # tag on each iteration.  Since we only need to consider each 
+    # tag combination once 
     reduced_set = tag_set.copy()   
     for tag1 in tag_set:
         reduced_set.remove(tag1)
+        # Compare the current tag with only those to which
+        # it hasn't already been compared.
         for tag2 in reduced_set:
             match_ratio = SequenceMatcher(a = tag1, b = tag2).ratio()
             if match_ratio > SIMILAR:
-                if len(tag2) < len(tag1):
-                    s = (tag2, tag1)
-                else:
-                    s = (tag1, tag2)
-                similiar_tags.append(s)
+                # Need to sort the tags so the shorter one is first
+                # ... probably not really necessary but it is required
+                #     to get the tests to pass ...
+                similiar_tags.append(sorted([tag1, tag2]))
     return similiar_tags
 
 if __name__ == "__main__":
