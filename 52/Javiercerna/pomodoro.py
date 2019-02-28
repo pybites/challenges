1f3ac43fb92ea60d45aeffd073dec24192870ce0
Javiercerna <javier.cerna@pucp.pe>
2019-02-27 20:00:56 -0500

    Let user specify Pomodoro time using args

--- 52/Javiercerna/pomodoro.py
+++ 52/Javiercerna/pomodoro.py
@@ -1,15 +1,35 @@
+import argparse
 import datetime
 import time
 import sys
 
 TIMER_END = datetime.timedelta(minutes=0)
 
-timer = datetime.timedelta(minutes=20)
 
-while timer != TIMER_END:
-    print(timer, end='\r', flush=True)
-    timer -= datetime.timedelta(seconds=1)
-    time.sleep(1)
+def countdown(timer_minutes=20):
+    timer = datetime.timedelta(minutes=timer_minutes)
+    while timer != TIMER_END:
+        print(timer, end='\r', flush=True)
+        timer -= datetime.timedelta(seconds=1)
+        time.sleep(1)
 
-print(TIMER_END)
-print('Pomodoro completed! Time for a break...')
+    print(TIMER_END)
+    print('Pomodoro completed! Time for a break...')
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser()
+    
+    parser.add_argument(
+        '--timer_minutes',
+        help='duration in minutes of timer (default is 20)',
+        type=int,
+        default=20
+    )
+
+    return parser.parse_args()
+
+
+if __name__ == '__main__':
+    args = parse_arguments()
+    countdown(args.timer_minutes)
