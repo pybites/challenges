55ece8f53735cf2b82398fb53b3aac258d14b858
Fabio Branquinho <branquif@gmail.com>
2019-03-26 21:10:26 -0300

    PCC52 branquif

--- 52/branquif/pomodoro.py
+++ 52/branquif/pomodoro.py
@@ -2,38 +2,35 @@
     Instructions + submit work: https://codechalleng.es/challenges/52"""
 
 from datetime import datetime, timedelta
+from decimal import *
 import time
 import signal
 import sys
-
-STEP_INTERVAL = 10
 
 def signal_handler(sig, frame):
     print('good bye!')
     sys.exit(0)
 
-def pomodoro(work_interval=20, rest_interval=5):
-    begin = datetime.now()
+def pomodoro(work_interval=20, rest_interval=5, step_interval=60):
     work = timedelta(minutes=work_interval)
     rest = timedelta(minutes=rest_interval)
+    interval = work
+    todo = "working"
     while True:
+        begin = datetime.now()
         while True:
             elapsed_time = datetime.now() - begin
-            print(f"working for minutes: {round(elapsed_time.seconds/60,0)} seconds: {elapsed_time.seconds}")
+            elapsed_minutes = Decimal(elapsed_time.seconds / 60).quantize(Decimal('1.'), rounding=ROUND_DOWN)
+            print(f"{todo} for minutes: {elapsed_minutes} seconds: {elapsed_time.seconds}")
             if elapsed_time >= work:
                 break
-            time.sleep(STEP_INTERVAL)
-        while True:
-            elapsed_time = datetime.now() - begin
-            print(f"resting for minutes: {round(elapsed_time.seconds/60,0)} seconds: {elapsed_time.seconds}")
-            if elapsed_time >= rest:
-                break
-            time.sleep(STEP_INTERVAL)
+            time.sleep(step_interval)
+        todo, interval = ("resting", rest) if todo == "working" else ("working", work)
 
 
 if __name__ == "__main__":
     signal.signal(signal.SIGINT, signal_handler)
-    pomodoro()
+    pomodoro(1,1,5)
 
 
 
