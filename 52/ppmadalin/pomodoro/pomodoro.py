6c63524781b77ba45c305e6f5a7db6d517136f63
Madalin Popa <ppmadalin@icloud.com>
2019-05-11 00:56:06 +0300

    update pomodoro cli

--- 52/ppmadalin/pomodoro/pomodoro.py
+++ 52/ppmadalin/pomodoro/pomodoro.py
@@ -1,13 +1,25 @@
 """
 pomodoro.py
-
-This is my pomodoro app cli version
-
 """
-from datetime import datetime
 from datetime import timedelta
 import time
 import os
+
+
+def countdown(seconds, message=None):
+    """
+    Countdown remainig time
+    """
+    while seconds >= 0:
+        if message:
+            print(message)
+        print(f'{seconds // 60}:{seconds % 60}')
+        time.sleep(1)
+        seconds -= 1
+        # clear prompt
+        os.system('cls' if os.name == 'nt' else 'clear')
+
+    return seconds
 
 
 def main():
@@ -17,18 +29,22 @@ def main():
     # clear the terminal at start
     os.system('cls' if os.name == 'nt' else 'clear')
 
-    now = datetime.now()
-    t = timedelta(minutes=1)
-    remaining_seconds = t.seconds
+    work = timedelta(minutes=1)
+    break_time = timedelta(minutes=1)
+    remaining_seconds = work.seconds
 
     try:
-        while remaining_seconds >= 1:
-            print(f"{remaining_seconds // 60}")
-            time.sleep(1)
-            remaining_seconds -= 1
-            os.system('cls' if os.name == 'nt' else 'clear')
+        while True:
+
+            remaining_seconds = countdown(remaining_seconds, "Start working...")
+
+            if remaining_seconds <= 0:
+                break_remaining_seconds = countdown(break_time.seconds, "Take a break for 5 minutes...")
+                remaining_seconds = work.seconds
+                continue
+
     except KeyboardInterrupt:
-        pass
+        print("Bye..")
 
 
 if __name__ == "__main__":
