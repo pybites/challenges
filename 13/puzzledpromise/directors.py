540800475887200e576359f2e80bc7a1b200cddb
Volkmar Petschnig <puzzled.promise@pm.me>
2019-05-26 19:54:38 +0200

    Finish work on challenge 13 - All tests pass

--- 13/puzzledpromise/directors.py
+++ 13/puzzledpromise/directors.py
@@ -1,32 +1,85 @@
 import csv
-import numpy as np
-from collections import Counter, defaultdict
-filename_movies = '../movie_metadata.csv'
+from collections import defaultdict, namedtuple
+from statistics import mean
 
-movies = defaultdict(list)
+MOVIE_DATA = 'movie_metadata.csv'
+NUM_TOP_DIRECTORS = 20
+MIN_MOVIES = 4
+MIN_YEAR = 1960
 
-with open(filename_movies,'r',encoding='utf-8') as csvfile:
-    reader = csv.DictReader(csvfile)
-    for r in reader:
-        if r['director_name'] == '' or r['title_year'] == '' or r['imdb_score'] == '':
+Movie = namedtuple('Movie', 'title year score')
+Director = namedtuple('Director', 'avg_rating movies')
+
+
+def get_movies_by_director():
+    '''Extracts all movies from csv and stores them in a dictionary
+    where keys are directors, and values is a list of movies (named tuples)'''
+    movies = defaultdict(list)
+
+    with open(MOVIE_DATA, 'r', encoding='utf-8') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for r in reader:
+            try:
+                if int(r['title_year']) >= MIN_YEAR:
+                    movies[r['director_name']].append(Movie(r['movie_title'], r['title_year'], r['imdb_score']))
+            except ValueError:
+                continue
+    return movies
+
+
+def get_average_scores(directors):
+    '''Filter directors with < MIN_MOVIES and calculate averge score.
+    According to the tests the returned result should be a dictionary
+    with a tuple consisting of (director_name, avg_score) as the key
+    and the movie list as the value'''
+
+    ratings = dict()
+    for director, movies in directors.items():
+        if director == '':
             continue
-        else:
-            if int(r['title_year']) >= 1960:
-                if r not in movies[r['director_name']]:
-                    movies[r['director_name']].append(r)
+        filtered_movies = []
+        for movie in movies:
+            if movie.title == '':
+                continue
+            if movie.score == '':
+                continue
 
-ratings = Counter()
-for d,m in movies.items():
-    if len(m) >= 4:
-        ratings[d] = np.mean([float(x['imdb_score']) for x in m])
+            filtered_movies.append(movie)
 
-rank = 1
-for director, score in ratings.most_common(20):
-    print(f"{rank:0=2d}. {director: <52} {score:.1f}")
-    print('------------------------------------------------------------')
-    for movie in sorted(movies[director], key=lambda x : float(x['imdb_score']), reverse=True):
-        imdb_score = float(movie['imdb_score'])
-        print(f"{movie['title_year']}] {movie['movie_title']: <51}{imdb_score:.1f}")
-    print()
-    rank += 1
+        if len(filtered_movies) >= MIN_MOVIES:
+            ratings[director, _calc_mean(filtered_movies)] = filtered_movies
+    return ratings
 
+
+def _calc_mean(movies):
+    '''Helper method to calculate mean of list of Movie namedtuples'''
+    return round(mean([float(x.score) for x in movies]), 1)
+
+def print_results(directors):
+    '''Print directors ordered by highest average rating. For each director
+    print his/her movies also ordered by highest rated movie.
+    See http://pybit.es/codechallenge13.html for example output'''
+
+    sorted_directors = sorted(directors.items(), key=lambda x : x[0][1], reverse=True)
+    sep_line = '-' * 60
+    counter = 1
+    for director, movies in sorted_directors[:NUM_TOP_DIRECTORS]:
+        print(f'{counter:0=2d}. {director[0]:<52} {director[1]}')
+        print(sep_line)
+        for m in sorted(movies, key= lambda x : x.score, reverse=True):
+            print(f'{m.year}] {m.title:<50} {m.score}')
+        print()
+        counter += 1
+
+
+
+def main():
+    '''This is a template, feel free to structure your code differently.
+    We wrote some tests based on our solution: test_directors.py'''
+    directors = get_movies_by_director()
+    directors = get_average_scores(directors)
+    print_results(directors)
+
+
+if __name__ == '__main__':
+    main()
