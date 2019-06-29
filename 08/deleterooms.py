db40d087d25a5f6676908d4b11f9e5a950c618cc
Jason <jcastle13@users.noreply.github.com>
2019-06-29 11:08:46 -0700

    Revert "Initial"

--- 08/deleterooms.py
+++
@@ -1,4 +0,0 @@
-def delete(rooms):
-    name = input("Enter name of room: ")
-    del rooms[name]
-    return rooms
--- 08/viewroom.py
+++
@@ -1,8 +0,0 @@
-def view(rooms, name):
-    print(name+": ")
-    
-    objects = [item for item in rooms[name][::2]]
-    prices = [item for item in rooms[name][1::2]]
-
-    for i, j in zip(objects, prices):
-        print("The item {} cost £{}".format(i, j))
