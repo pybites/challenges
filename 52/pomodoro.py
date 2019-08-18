48eefcdc0ff246e56ddb2d903dd2818d1097c3c9
matteobrv <matteosulweb@gmail.com>
2019-08-18 17:17:33 +0200

    add interval_countdown function

--- 52/pomodoro.py
+++ 52/pomodoro.py
@@ -9,3 +9,17 @@ import argparse
 # default interval and break length values
 INTERVAL_LENGTH = timedelta(minutes=25)
 BREAK_LENGTH = timedelta(minutes=5)
+
+
+def interval_countdown(sessions):
+	'''Timer-like function to keep track of a single session.
+
+	Args:
+		param1 (int): number of sessions left.
+	'''
+	interval = INTERVAL_LENGTH
+	while interval:
+		interval -= timedelta(seconds=1)
+		sleep(1)
+		remaining_sessions = f'Remaininig sessions: {sessions}'
+		print(f'{interval} - {remaining_sessions}\t\t\t', end='\r')
