a8959b91bfc10a9c8b1427a4e09170b74f2db295
matteobrv <matteosulweb@gmail.com>
2019-08-18 17:14:38 +0200

    import necessary modules

--- 52/pomodoro.py
+++ 52/pomodoro.py
@@ -1,2 +1,7 @@
 """ PCC52 - Create your own Pomodoro Timer (24th of Sept 2018)
     Instructions + submit work: https://codechalleng.es/challenges/52"""
+
+from datetime import timedelta
+from time import sleep
+import argparse
+
