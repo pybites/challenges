c42a706bf7d074ecd077954ac20b74c31e2b7225
Chris Patti <feoh@feoh.org>
2019-09-04 18:28:56 -0400

    Added all neceessary tests from challenge. Refactored for better readability and reuse.

--- 32/feoh/test_pybites_selenium.py
+++ 32/feoh/test_pybites_selenium.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 
 pyplanet_home_url = "https://pyplanet.herokuapp.com/"
+pyplanet_app_url = pyplanet_home_url + "pyplanet/"
 
 def find_pyplanet_link(driver):
     assert driver.find_elements_by_link_text("Login")
@@ -18,9 +19,13 @@ def find_table(driver):
     assert th.text == "Title"
     return tbl
 
+def find_first_article_link(table):
+    return table.find_element_by_xpath("/html/body/main/table/tbody/tr[1]/td/a")
 
-def test_logged_out_view():
-    driver = webdriver.Chrome()  # replaced Firefox by Chrome
+def init_driver():
+    return webdriver.Chrome()  # replaced Firefox by Chrome
+
+def test_logged_out_view(driver):
     driver.get("https://pyplanet.herokuapp.com/")
     assert "PyBites 100 Days of Django" in driver.title
 
@@ -29,20 +34,26 @@ def test_logged_out_view():
     pyplanet_link.click()
 
     tbl = find_table(driver)
-
     assert len(tbl.find_elements_by_tag_name("tr")) - 1 == 100
-    first_article_link = tbl.find_element_by_xpath("/html/body/main/table/tbody/tr[1]/td/a")
-    article_name = first_article_link.text
 
+    first_article_link = find_first_article_link(tbl)
+
+    first_article_name = first_article_link.text
+    first_article_url = first_article_link.get_attribute("href")
     first_article_link.click()
     main_div = driver.find_element_by_tag_name("main")
     article_header = main_div.find_element_by_tag_name("h2")
-    assert article_name == article_header.text
+    assert first_article_name == article_header.text
 
     go_back = driver.find_elements_by_link_text("Go back")[0]
     homepage_link = go_back.get_attribute("href")
-    assert homepage_link == pyplanet_home_url + "pyplanet/"
+    assert homepage_link == pyplanet_app_url
 
+    return first_article_url
+
+
+
+def test_logged_in_view(driver, first_article_url):
     login_link = driver.find_elements_by_link_text("Login")[0]
     login_link.click()
 
@@ -56,9 +67,41 @@ def test_logged_out_view():
     submit.click()
     assert driver.current_url == pyplanet_home_url
 
-    return driver
+    assert driver.find_element_by_link_text("Logout")
+    assert driver.find_element_by_link_text("Home")
+    login_div = driver.find_element_by_id("login")
+    assert "Welcome back, guest!" in login_div.text
+
+    validate_tweet_this(driver, first_article_url)
+
+def validate_tweet_this(driver, article_url):
+    driver.get(article_url)
+    assert driver.find_element_by_link_text("Tweet this")
+
+
+def validate_no_logged_out_tweets_for_you(driver, article_url):
+    driver.get(article_url)
+    # This doesn't work. Apparently the correct way is find_elements_by() - Gotta figure that out. Maybe. Someday :)
+    assert not driver.find_element_by_link_text("Tweet this")
+
+
+def validate_all_quiet_after_logout(driver):
+    logout_link = driver.find_element_by_link_text("Logout")
+    logout_link.click()
+    header = driver.find_element_by_tag_name("h1")
+    assert header.text == "See you!"
+
+    para = driver.find_element_by_tag_name("p")
+    assert para.text == "You have been successfully logged out."
+
 
 if __name__ == "__main__":
-    driver = test_logged_out_view()
+    driver = init_driver()
+    first_article_url = test_logged_out_view(driver)
+    test_logged_in_view(driver, first_article_url)
+    validate_all_quiet_after_logout(driver)
+    # Attempt at a stretch goal. Not working :)
+    # validate_no_logged_out_tweets_for_you(driver, first_article_url)
 
     driver.close()
+    print("YAY! Test suite succeeded!")
