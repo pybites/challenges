facf48b716a4fc19b74542fdc2e919ef440d3fbb
Benson <bigbuzi@gmail.com>
2019-09-18 06:43:10 -0400

    Added in parsing code

--- 13/bigarabuza/directors.py
+++ 13/bigarabuza/directors.py
@@ -1,9 +1,37 @@
-from pathlib import Path
 import csv
+from pathlib import Path
+from collections import namedtuple, defaultdict, Counter
 
 movie_csv = Path(__file__).resolve().parents[1] / 'movie_metadata.csv'
 
-with open(movie_csv) as f:
-    for line in csv.DictReader(f):
-        print(line)
+Movie = namedtuple('Movie', 'title year score')
+
+def get_movies_by_director(data=movie_csv):
+    
+    directors = defaultdict(list)
+
+    with open(data, encoding='utf-8') as f: 
+        for line in csv.DictReader(f):
+            try:
+                director = line['director_name']
+                movie = line['movie_title'].replace('\xa0', '')
+                year = int(line['title_year'])
+                score = float(line['imdb_score'])
+            except ValueError:
+                continue
+            
+            m = Movie(title=movie, year=year, score=score)
+            directors[director].append(m)
+
+    return directors
+
+directors = get_movies_by_director()
+
+cnt = Counter()
+for director, movies in directors.items():
+    cnt[director] += len(movies)
+
+print(cnt.most_common(5))
+
+
 
