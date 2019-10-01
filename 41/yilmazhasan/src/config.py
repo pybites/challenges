0596545b816e4fe8dd001f6d587637efd64c1328
yilmazhasan <yilmazhasan111@gmail.com>
2019-10-02 02:31:53 +0300

    update

--- 41/yilmazhasan/src/config.py
+++ 41/yilmazhasan/src/config.py
@@ -5,10 +5,6 @@ consumer_key = ""
 consumer_secret = ""
 access_token = ""
 access_token_secret = ""
-consumer_key = "3yRNSjK2EvlDPa91URqvx2UXh"
-consumer_secret = "tTwbTLxkW4ee2hQPPJrN12IFZr4QKk9cxSG8pty5APAzwvwqTN"
-access_token = "476723317-tAVQDJIgnyI92GPd0T9kwyUAimzHbbZccUX8doCC"
-access_token_secret = "5CVyrH386j9psmckgwbzhMrHSta18vnL0wntjx7FGCxb1"
 databasename = "tipdatabase.db"
 
 
