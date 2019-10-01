07308ac77ec4dfd57582be6514c2f03617f05939
hasan yılmaz <yilmazhasan111@outlook.com>
2019-10-01 21:06:46 +0300

    rename

--- 40/yilmazhasan/app/urls.py
+++ 40/yilmazhasan/app/urls.py
@@ -1,4 +1,4 @@
-"""mysite URL Configuration
+"""app URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/2.2/topics/http/urls/
--- 40/yilmazhasan/app/wsgi.py
+++ 40/yilmazhasan/app/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for mysite project.
+WSGI config for app project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'mysite.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'app.settings')
 
 application = get_wsgi_application()
