3de079e0845d398e2a5d21891fe2982ab518f279
Vuottek Un <30956131+userinfamous@users.noreply.github.com>
2019-10-02 17:12:02 +0700

    Refactored PCC01 (#592)
    * Refactored PCC01

--- 01/userinfamous/wordvalue.py
+++ 01/userinfamous/wordvalue.py
@@ -1,5 +1,8 @@
+#Imports and appending path to reach beyond parent dir
+import os, sys
+sys.path.append('..')
 from data import LETTER_SCORES, DICTIONARY 
-import os
+
 
 #File path for Dictionary
 fname = os.path.join('../',DICTIONARY)
@@ -7,27 +10,21 @@ fname = os.path.join('../',DICTIONARY)
 #Wordlist from dictionary
 def load_words() -> list:
     with open(fname,'r') as f:
-        wordlist = f.read().split()
-        return wordlist
+        wordlist = f.read().split('\n')
+    return wordlist[:-1]
 
+#Calculate score word for word
 def calc_word_value(word:str) -> int:
-    score = 0
-    for letter in word:
-        if letter.upper() in LETTER_SCORES:
-            score += LETTER_SCORES[letter.upper()]
+    score = sum(LETTER_SCORES.get(letter.upper(),0) for letter in word)
     return score
 
-def max_word_value(words=None):
+#Just get the max out of the word list (any list)
+def max_word_value(words:list=None) -> str:
     wordvalue_pair = list()
     #checking if given argument
-    if words == None:
-        wordlist = load_words()
-    else:
-        wordlist = words
+    wordlist = words if words else load_words()
     for word in wordlist:
         score = calc_word_value(word)
         wordvalue_pair.append( (word,score) )
     result = max(wordvalue_pair, key=lambda x: x[1])
     return result[0]
-
-
