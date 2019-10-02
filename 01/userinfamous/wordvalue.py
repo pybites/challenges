a62e3ded3b37b64d60ce39edc9bd5361bf62ed52
Virento Voshe <30956131+userinfamous@users.noreply.github.com>
2019-10-02 10:35:29 +0700

    Refactored PCC01

--- 01/userinfamous/wordvalue.py
+++ 01/userinfamous/wordvalue.py
@@ -10,25 +10,19 @@ fname = os.path.join('../',DICTIONARY)
 #Wordlist from dictionary
 def load_words() -> list:
     with open(fname,'r') as f:
-        wordlist = f.read().split()
-    return wordlist
+        wordlist = f.read().split('\n')
+    return wordlist[:-1]
 
 #Calculate score word for word
 def calc_word_value(word:str) -> int:
-    score = 0
-    for letter in word:
-        if letter.upper() in LETTER_SCORES:
-            score += LETTER_SCORES[letter.upper()]
+    score = sum(LETTER_SCORES.get(letter.upper(),0) for letter in word)
     return score
 
 #Just get the max out of the word list (any list)
 def max_word_value(words:list=None) -> str:
     wordvalue_pair = list()
     #checking if given argument
-    if words == None:
-        wordlist = load_words()
-    else:
-        wordlist = words
+    wordlist = words if words else load_words()
     for word in wordlist:
         score = calc_word_value(word)
         wordvalue_pair.append( (word,score) )
