7b1c7626ebe30c542bd4392dbd3b1639296f3e34
Rhys <rhys.powell@tcs.com>
2019-10-03 16:15:30 +0100

    add part 2

---
+++ 02/rhys/game.py
@@ -0,0 +1,62 @@
+#!python3
+# Code Challenge 02 - Word Values Part II - a simple game
+# http://pybit.es/codechallenge02.html
+
+import random
+
+from data import DICTIONARY, LETTER_SCORES, POUCH
+
+NUM_LETTERS = 7
+
+
+# re-use from challenge 01
+def calc_word_value(word):
+    """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
+    return sum(LETTER_SCORES.get(char.upper(), 0) for char in word)
+
+
+# re-use from challenge 01
+def max_word_value(words):
+    """Calc the max value of a collection of words"""
+    return max(words, key=calc_word_value)
+
+def process_word(letters):
+    while True:
+        word = input("Enter your word\n").upper()
+        print(f'your word is {word}\n')
+        print(set(letters))
+        print(set(word))
+        print(len(letters)-len(word))
+        print((len(letters)-len(word))+len(word))
+        if len(word) == 0:
+            print("Couldn't get a word? Want to quit? If you do press y")
+            quit = input("y or n")
+            if quit == "y":
+                exit()
+        elif len(word) > 7:
+            print("You need to use the letters shown")
+            print(*letters)
+        elif set(letters).issuperset(set(word)) and (len(set(letters))-len(set(word)))+len(word) == 7:
+            print('Good word')
+            break
+        else:
+            print("its broken You need to use the letters shown")
+            print(*letters)
+
+    return(word)
+
+
+def main():
+    letters = random.sample(POUCH, NUM_LETTERS)
+    print(*letters)
+    get_word = process_word(letters)
+    word = input("Enter your word")
+    if word in DICTIONARY:
+        print("congratulations you have chosen a correct word")
+        score = calc_word_value(word)
+    
+
+
+
+if __name__ == "__main__":
+    main()
