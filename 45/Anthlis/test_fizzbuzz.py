fc47c3a0bde9c842b5d8c2d9c2faac9a195146e7
Anthony Lister <anthony.lister@gmail.com>
2019-10-05 19:21:17 +1300

    PCC45 Anthlis

---
+++ 45/Anthlis/test_fizzbuzz.py
@@ -0,0 +1,33 @@
+import pytest
+import mock
+
+from fizzbuzz import fizzbuzz
+
+@pytest.mark.parametrize("test_input, expected",[
+    (1, 1),
+    (2, 2),
+    (3, 'Fizz'),
+    (4, 4),
+    (5, 'Buzz'),
+    (6, 'Fizz'),
+    (7, 7),
+    (8, 8),
+    (9, 'Fizz'),
+    (10, 'Buzz'),
+    (11, 11),
+    (12, 'Fizz'),
+    (13, 13),
+    (14, 14),
+    (15, 'Fizz Buzz'),
+    (16, 16),
+    (50, 'Buzz'),
+    (60, 'Fizz Buzz'),
+    (74, 74),
+    (75, 'Fizz Buzz'),
+    (92, 92),
+    (98, 98),
+    (99, 'Fizz'),
+    (100, 'Buzz'),
+])
+def test_fizzbuzz(test_input, expected):
+    assert fizzbuzz(test_input) == expected
