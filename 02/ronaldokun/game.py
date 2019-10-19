52397be7c57dcb8c7005e2ee111545e0d3e1b8ba
Ronaldo <ronaldokun@gmail.com>
2019-10-19 18:04:22 -0300

    Refactor file to conform to test

--- 02/ronaldokun/game.py
+++ 02/ronaldokun/game.py
@@ -13,6 +13,9 @@ from data import DICTIONARY, LETTER_SCORES, POUCH
 
 NUM_LETTERS = 7
 
+def draw_letters():
+    """Pick NUM_LETTERS letters randomly. Hint: use stdlib random"""
+    return choices(POUCH, k=NUM_LETTERS)
 # re-use from challenge 01
 def calc_word_value(word):
     """Calc a given word value based on Scrabble LETTER_SCORES mapping"""
@@ -25,7 +28,7 @@ def max_word_value(words):
     return max(words, key=calc_word_value)
 
 
-def validate(word: str, draw: list) -> bool:
+def _validation(word: str, draw: list) -> bool:
     """Check if word can be formed from letters in draw
     :param word: word to be checked
     :param draw: set of letters to form a word
@@ -35,10 +38,12 @@ def validate(word: str, draw: list) -> bool:
     for c in word:
         try:
             copy.remove(c)
-        except ValueError:
-            return False
-    else:
-        return True
+        except ValueError as e:
+            raise ValueError(f"The word {word} cannot be formed from the {draw}") from e
+    if not in_dict(word):
+        raise ValueError(f"The word {word} is not a valid one")
+
+    return True
 
 
 def in_dict(word: str) -> bool:
@@ -48,40 +53,46 @@ def in_dict(word: str) -> bool:
     """
     return "".join(word).lower() in DICTIONARY
 
+def input_word(draw):
+    """Ask player for a word and validate against draw.
+    Use _validation(word, draw) helper."""
+    word = input("Insert a valid word: ").upper()
+    if _validation(word, draw):
+        return word
+
+# Below 2 functions pass through the same 'draw' argument (smell?).
+# Maybe you want to abstract this into a class?
+# get_possible_dict_words and _get_permutations_draw would be instance methods.
+# 'draw' would be set in the class constructor (__init__).
+def get_possible_dict_words(draw):
+    """Get all possible words from draw which are valid dictionary words.
+    Use the _get_permutations_draw helper and DICTIONARY constant"""
+    return list(filter(in_dict, _get_permutations_draw(draw)))
+
+
+def _get_permutations_draw(draw):
+    """Helper for get_possible_dict_words to get all permutations of draw letters.
+    Hint: use itertools.permutations"""
+    return (w for k in range(1, NUM_LETTERS+1) for w in permutations(draw, k))
+
 
 def main():
 
-    draw = choices(POUCH, k=NUM_LETTERS)
-
+    draw = draw_letters()
     print(f"Letters drawn: {draw}")
 
-    word = input("Insert a valid word: ").upper()
+    word = input_word(draw)
 
-    if not validate(word, draw):
-        print(f"The word {word} cannot be formed from the {draw}")
-        return
+    word_score = calc_word_value(word)
+    print(f"Word Chosen: {word} (Value: {word_score})")
 
-    if not in_dict(word):
-        print(f"The word {word} is not a valid one")
-        return
+    possible_words = get_possible_dict_words(draw)
 
-    val_word = calc_word_value(word)
-    print(f"Word Chosen: {word} (Value: {val_word})")
-
-    k = NUM_LETTERS
-
-    while k >= 0:
-        max_word = sorted(filter(in_dict, permutations("".join(draw), k)), key=calc_word_value, reverse=True)
-        if max_word:
-            break
-        else:  # there is not a word from this draw and NUM_LETTERS
-            k -= 1
-
-    max_word = "".join(max_word[0])
+    max_word = max_word_value(possible_words)
     val_max_word = calc_word_value(max_word)
 
     print(f"The Optimal Word for this Draw is {''.join(max_word)} (Value: {val_max_word})")
-    print(f"Your Score (player_score / optimal_score) : {val_word / val_max_word: .2f}")
+    print(f"Your Score (player_score / optimal_score) : {word_score / val_max_word * 100: .2f}")
 
 
 if __name__ == "__main__":
