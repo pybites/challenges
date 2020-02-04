387a9718e70bdbe0bc2d0b07e072655cae2e44c5
Maciec <maciekkuc1@gmail.com>
2020-02-04 22:16:49 +0100

    Chalenge completed
    Implemented the searching algorithm.

--- 01/maciekKuc/wordvalue.py
+++ 01/maciekKuc/wordvalue.py
@@ -1,12 +1,20 @@
-from data import DICTIONARY, LETTER_SCORES
+from data import DICTIONARY, scrabble_scores
 
 #function for loading words
 
 def load_words():
     with open(DICTIONARY, 'r') as file:
         words = file.read().split('\n')
-        print(len(words))
-        for line in file:
-            print(line)
+        score = [0,'']
+        for word in words:
+            actuall_score = 0
+            for letter in word:
+                for scores in scrabble_scores:
+                    if letter.upper() in scores[1]:
+                        actuall_score = actuall_score + scores[0]
+            if actuall_score > score[0]:
+                score = [actuall_score, word]
+        print('The highest score is %s and its for %s' % (score[0], score[1]))
+        return score
 
 load_words()
