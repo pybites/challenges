d03eda8648844eb3ffddf48b5f97eef774941654
Robin Beer <r.beer@outlook.de>
2020-02-22 23:34:22 +0100

    implemented first idea

---
+++ 11/Zaubeerer/generators-template.py
@@ -0,0 +1,57 @@
+"""
+Turn the following unix pipeline into Python code using generators
+
+$ for i in ../*/*py; do grep ^import $i|sed 's/import //g' ; done | sort | uniq -c | sort -nr
+   4 unittest
+   4 sys
+   3 re
+   3 csv
+   2 tweepy
+   2 random
+   2 os
+   2 json
+   2 itertools
+   1 time
+   1 datetime
+"""
+
+import glob, os
+import re
+
+import collections
+
+def gen_files(pat):
+    """Finds all files"""
+
+    for file in glob.glob(pat):
+        yield file
+
+def gen_lines(files):
+    """Finds all lines"""
+    
+    for file in files:
+        with open(file) as f:
+            yield f.readline()
+
+def gen_grep(lines, pattern):
+    """Analyze whether lines match a certain pattern"""
+    for line in lines:
+        if line.startswith(pattern):
+            yield line[len(pattern):]
+
+def gen_count(lines):
+    """Count number of occurrences of specific patterns"""
+
+    yield collections.Counter(line for line in lines)
+        
+
+if __name__ == "__main__":
+    # call the generators, passing one to the other
+    file_pattern = '../*/*.py'
+    line_pattern = "import "
+ 
+    files = gen_files(file_pattern)
+    lines = gen_lines(files)
+    lines_of_interest = gen_grep(lines, line_pattern)
+    counted_lines = gen_count(lines_of_interest)
+
