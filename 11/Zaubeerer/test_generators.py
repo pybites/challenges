1aa493607c87828a2f4cf5c5165a1625b19e6dfb
Robin Beer <r.beer@outlook.de>
2020-02-23 15:51:56 +0100

    added test_integrated() to test generator chain

--- 11/Zaubeerer/test_generators.py
+++ 11/Zaubeerer/test_generators.py
@@ -41,3 +41,15 @@ def test_gen_count():
     expected_counts = [Counter({'generators': 2, 'numpy': 1})]
 
     assert list(generators.gen_count(lines)) == expected_counts
+
+def test_integrated():
+
+    expected_counter = Counter({'re': 8, 'glob': 6, 'collections': 2, 'sys': 1, 'glob, os': 1, 'generators': 1, 'ast': 1})
+
+    files = generators.gen_files("../*/*.py")
+
+    lines = generators.gen_lines(files)
+    lines_of_interest = generators.gen_grep(lines, LINE_PATTERN)
+    counted_lines = generators.gen_count(lines_of_interest)
+
+    assert list(counted_lines)[0] == expected_counter
