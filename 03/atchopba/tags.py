2396a6dca0802411491bbd35c5a37f462228516b
atchopba <atchopba@gmail.com>
2020-04-03 21:45:45 +0200

    PCC #03

---
+++ 03/atchopba/tags.py
@@ -0,0 +1,56 @@
+from collections import Counter
+from difflib import SequenceMatcher
+from itertools import product
+import re
+
+IDENTICAL = 1.0
+TOP_NUMBER = 10
+RSS_FEED = 'rss.xml'
+SIMILAR = 0.87
+TAG_HTML = re.compile(r'<category>([^<]+)</category>')
+
+
+def get_tags():
+    """Find all tags (TAG_HTML) in RSS_FEED.
+    Replace dash with whitespace.
+    Hint: use TAG_HTML.findall"""
+    str_file = ''
+    with open(RSS_FEED,'r') as f:
+        str_file += f.read().strip()
+    tags = TAG_HTML.findall(str_file)
+    return [tag.replace("-", " ") for tag in tags]
+
+
+def get_top_tags(tags):
+    """Get the TOP_NUMBER of most common tags
+    Hint: use most_common method of Counter (already imported)"""
+    return sorted(Counter(tags).items(), key=lambda item: item[1], reverse=True)[:TOP_NUMBER]
+
+
+def get_similarities(tags):
+    """Find set of tags pairs with similarity ratio of > SIMILAR
+    Hint 1: compare each tag, use for in for, or product from itertools (already imported)
+    Hint 2: use SequenceMatcher (imported) to calculate the similarity ratio
+    Bonus: for performance gain compare the first char of each tag in pair and continue if not the same"""
+    similarities = []
+    for tag_1 in tags:
+        for tag_2 in tags:
+            if tag_1 != tag_2:
+                s=SequenceMatcher(lambda x: x == " ", tag_1, tag_2)
+                if round(s.ratio(), 2) > SIMILAR:
+                    similarities.append(sorted([tag_1, tag_2]))
+                    tags.remove(tag_2)
+    return similarities
+
+
+if __name__ == "__main__":
+    tags = get_tags()
+    top_tags = get_top_tags(tags)
+    print('* Top {} tags:'.format(TOP_NUMBER))
+    for tag, count in top_tags:
+        print('{:<20} {}'.format(tag, count))
+    similar_tags = dict(get_similarities(tags))
+    print()
+    print('* Similar tags:')
+    for singular, plural in similar_tags.items():
+        print('{:<20} {}'.format(singular, plural))
