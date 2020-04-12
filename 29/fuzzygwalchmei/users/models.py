8b83fd533e15d98520e99548fce6895315dea6d8
fuzzygwalchmei <marc.falzon@gmail.com>
2020-04-12 22:34:13 +1000

    PCC29 Fuzzygwalchmei

---
+++ 29/fuzzygwalchmei/users/models.py
@@ -0,0 +1,9 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField()
+
+    def __str__(self):
+        return f'{self.user.username} Profile'
