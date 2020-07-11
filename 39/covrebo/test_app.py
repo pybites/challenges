04dd43d7b2dbd88c9f0c2a15330e1128cf149c52
Chris Ovrebo <chris.ovrebo@gmail.com>
2020-07-11 08:54:31 -0500

    Split larger tests into smaller tests to provide better output

--- 39/covrebo/test_app.py
+++ 39/covrebo/test_app.py
@@ -7,8 +7,7 @@ url = 'http://127.0.0.1:5000'
 
 @pytest.fixture
 def client():
-    client = app.test_client()
-    return client
+    return app.test_client()
 
 def test_get_item():
     assert _get_item(1) == [{'id': 1, 'name': 'laptop', 'value': 1000}]
@@ -26,53 +25,36 @@ def test_record_exists():
 
 @pytest.fixture
 def client():
-    client = app.test_client()
-    return client
+    return app.test_client()
 
-def test_get_items():
-    res = requests.get(url + '/api/v1.0/items')
+def test_get_items(client):
+    res = client.get(url + '/api/v1.0/items')
     assert res.status_code == 200
 
-    data = res.json()
+    data = json.loads(res.get_data())
     assert data['items'][0]['id'] == 1
     assert data['items'][1]['id'] == 2
     assert data['items'][2]['id'] == 3
 
-def test_get_item():
+def test_get_item(client):
+
     # Verify a successful connection
-    res = requests.get(url + '/api/v1.0/items/1')
+    res = client.get(url + '/api/v1.0/items/1')
     assert res.status_code == 200
 
     # Verify correct data
-    data = res.json()
+    data = json.loads(res.get_data())
     assert data['items'][0]['id'] == 1
 
+
+def test_get_item_not_in_list(client):
+
     # Check for a item outside the list
-    res = requests.get(url + '/api/v1.0/items/42')
+    res = client.get(url + '/api/v1.0/items/42')
     assert res.status_code == 404
 
 
 def test_create_item(client):
-    # check for 400 return if id is missing
-    res = requests.post(url + '/api/v1.0/items', data=json.dumps({'name': 'lamp', 'value': 5000}))
-    assert res.status_code == 400
-
-    # check for 400 return if value is missing
-    res = requests.post(url + '/api/v1.0/items', data=json.dumps({'id': '4', 'value': 5000}))
-    assert res.status_code == 400
-
-    # return 400 if data is not json
-    input = {'name': 'lamp', 'value': 250}
-    res = client.post(url + '/api/v1.0/items', data=input, content_type='application/json')
-    assert res.status_code == 400
-
-    # check for 400 if name already exists
-    res = requests.post(url + '/api/v1.0/items', data=json.dumps({'id': '4', 'name ': 'laptop', 'value': 5000}))
-    assert res.status_code == 400
-
-    # check for 400 if value is not an int
-    res = requests.post(url + '/api/v1.0/items', data=json.dumps({'id': '4', 'name ': 'laptop', 'value': 'Forty-two'}))
-    assert res.status_code == 400
 
     # check for the correct addition of an item
     input = {'name': 'lamp', 'value': 250}
@@ -83,20 +65,36 @@ def test_create_item(client):
     data = json.loads(res.get_data())
     assert data['item']['name'] == 'lamp'
 
-def test_update_item(client):
-    # return 404 if id is not given
-    input = {'id': '', 'name': 'lamp', 'value': 5000}
-    res = client.put(url + '/api/v1.0/items/', data=json.dumps(input), content_type='application/json')
-    assert res.status_code == 404
 
+def test_create_item_missing_inputs(client):
+    # check for 400 return if id is missing
+    res = client.post(url + '/api/v1.0/items', data=json.dumps({'name': 'lamp', 'value': 5000}))
+    assert res.status_code == 400
+
+    # check for 400 return if value is missing
+    res = client.post(url + '/api/v1.0/items', data=json.dumps({'id': '4', 'value': 5000}))
+    assert res.status_code == 400
+
+
+def test_create_item_correct_data_type(client):
     # return 400 if data is not json
-    input = {'id': '', 'name': 'lamp', 'value': 5000}
-    res = client.put(url + '/api/v1.0/items/', data=input, content_type='application/json')
-    assert res.status_code == 404
+    input = {'name': 'lamp', 'value': 250}
+    res = client.post(url + '/api/v1.0/items', data=input, content_type='application/json')
+    assert res.status_code == 400
 
     # check for 400 if value is not an int
-    res = client.put(url + '/api/v1.0/items/1', data=json.dumps({'id': '1', 'name ': 'laptop', 'value': 'Forty-two'}))
+    res = client.post(url + '/api/v1.0/items', data=json.dumps({'id': '4', 'name ': 'laptop', 'value': 'Forty-two'}))
     assert res.status_code == 400
+
+
+def test_create_item_duplicate_name(client):
+
+    # check for 400 if name already exists
+    res = client.post(url + '/api/v1.0/items', data=json.dumps({'id': '4', 'name ': 'laptop', 'value': 5000}))
+    assert res.status_code == 400
+
+
+def test_update_item(client):
 
     # check for the correct update of an item
     input = {'id': '1', 'name': 'lamp', 'value': 250}
@@ -107,15 +105,38 @@ def test_update_item(client):
     data = json.loads(res.get_data())
     assert data['item']['name'] == 'lamp'
 
-def test_delete_item(client):
+
+def test_update_item_missing_id(client):
     # return 404 if id is not given
     input = {'id': '', 'name': 'lamp', 'value': 5000}
-    res = client.delete(url + '/api/v1.0/items/', data=json.dumps(input), content_type='application/json')
+    res = client.put(url + '/api/v1.0/items/', data=json.dumps(input), content_type='application/json')
     assert res.status_code == 404
+
+
+def test_update_correct_data_input(client):
+    # return 400 if data is not json
+    input = {'id': '', 'name': 'lamp', 'value': 5000}
+    res = client.put(url + '/api/v1.0/items/', data=input, content_type='application/json')
+    assert res.status_code == 404
+
+    # check for 400 if value is not an int
+    res = client.put(url + '/api/v1.0/items/1', data=json.dumps({'id': '1', 'name ': 'laptop', 'value': 'Forty-two'}))
+    assert res.status_code == 400
+
+
+def test_delete_item(client):
 
     # check for the correct deletion of an item
     res = client.delete(url + '/api/v1.0/items/1')
     assert res.status_code == 204
 
     res = client.get(url + '/api/v1.0/items/1')
+    assert res.status_code == 404
+
+
+def test_delete_item_missing_id(client):
+
+    # return 404 if id is not given
+    input = {'id': '', 'name': 'lamp', 'value': 5000}
+    res = client.delete(url + '/api/v1.0/items/', data=json.dumps(input), content_type='application/json')
     assert res.status_code == 404
