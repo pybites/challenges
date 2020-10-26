9b571fd4ef5d138c72fee3a8dbc31b56676eba85
Geoff Riley <gir6@ou.ac.uk>
2020-10-26 17:50:34 +0000

    PCC52 Geoff Riley: a class act with tomatoes

--- 52/GeoffRiley/tomato.py
+++ 52/GeoffRiley/tomato.py
@@ -1,44 +1,23 @@
+"""
+     🍅 🍅 🍅 🍅 🍅 🍅 🍅 🍅
+    🍅   Pomodoro Timer  🍅
+     🍅 🍅 🍅 🍅 🍅 🍅 🍅 🍅
+"""
+from dataclasses import dataclass
+from enum import Enum
+from typing import Callable, Tuple
+
 import pygame
+import pygame.freetype
+import math
 from pygame.rect import Rect
 
 # scalar is used to scale time... it's the number of seconds for a
-# pomodoro minute
+# pomodoro minute: normal is 60 for real time; testing is 1 for fast time
 scalar = 1
 # pygame generic parameters
-WIDTH, HEIGHT = 800, 300
-FPS = 60
-
-# pygame essentials
-pygame.init()
-display = pygame.display.set_mode((WIDTH, HEIGHT))
-pygame.display.set_caption('Living with tomatoes')
-
-# Pomodoro time periods
-TIMES = {
-    'work': 25,
-    'rest': 5,
-    'long_rest': 10
-}
-TIME_TRANSITIONS = {
-    'work': ['rest', 'long_rest'],
-    'rest': ['work'],
-    'long_rest': ['work']
-}
-TIME_MESSAGES = {
-    'work': 'Program! Program!',
-    'rest': 'Cool your heels!',
-    'long_rest': 'Get the coffee in!'
-}
-LONG_REST_AFTER = 3
-pomodoro_status = 'work'
-pomodoro_rest_count = 0
-
-# timing elements
-target_time = TIMES[pomodoro_status] * scalar
-TIMER_STOPPED = 0
-TIMER_RUNNING = 1
-TIMER_PAUSED = 2
-timer_status = TIMER_STOPPED
+WIDTH, HEIGHT = 700, 300
+FPS = 50
 
 # colour setups
 BLACK = (0, 0, 0)
@@ -48,131 +27,213 @@ BLUE = (0, 0, 255)
 GREY = (127, 127, 127)
 WHITE = (255, 255, 255)
 
-# font declarations
-BUTTON_FONT = pygame.font.SysFont('sans', 30)
-TIMER_FONT = pygame.font.SysFont('sans', 100)
-MESSAGE_FONT = pygame.font.SysFont('serif', 60)
+
+class TimerStatus(Enum):
+    TIMER_STOPPED = 0
+    TIMER_RUNNING = 1
+    TIMER_PAUSED = 2
 
 
-# pomodoro specific declarations
-def do_start():
-    global timer_status
-    if timer_status == TIMER_RUNNING:
-        return
-    pygame.time.set_timer(pygame.USEREVENT, 1000)
-    timer_status = TIMER_RUNNING
+@dataclass
+class Point:
+    x: int
+    y: int
 
+    @property
+    def xy(self) -> Tuple[int, int]:
+        return self.x, self.y
 
-def do_pause():
-    global timer_status
-    if timer_status == TIMER_RUNNING:
-        pygame.time.set_timer(pygame.USEREVENT, 0)
-        timer_status = TIMER_PAUSED
-
-
-def do_reset():
-    global pomodoro_status, pomodoro_rest_count, target_time, timer_status
-    if timer_status == TIMER_STOPPED:
-        return
-    pygame.time.set_timer(pygame.USEREVENT, 0)
-    pomodoro_status = 'work'
-    pomodoro_rest_count = 0
-    target_time = TIMES[pomodoro_status] * scalar
-    timer_status = TIMER_STOPPED
-
-
-# buttons
-BUTTONS = [
-    ('START', (GREEN, GREY, GREEN), do_start),
-    ('PAUSE', (GREY, BLUE, GREY), do_pause),
-    ('RESET', (GREY, RED, RED), do_reset)
-]
-BUTTON_WIDTH, BUTTON_HEIGHT = 90, 40
-BUTTON_COUNT = 3
-GAP = 100
-button_pos = []
 
 # timer text
-TICK_TEXT_CENTER = (WIDTH // 2, 75)
+TICK_TEXT_CENTER = Point(WIDTH // 2, 75)
 TICK_TEXT_COLOUR = RED
 
 
-def init_button_positions():
-    global button_pos
+@dataclass
+class PomButton:
+    text: str
+    colour_map: Tuple[Tuple[int, int, int], Tuple[int, int, int], Tuple[int, int, int]]
+    action: Callable[[], None]
+    button_pos: Point
+    width: int = 90
+    height: int = 40
 
-    button_left = (WIDTH - BUTTON_COUNT * BUTTON_WIDTH - GAP * (BUTTON_COUNT - 1)) // 2
-    button_top = HEIGHT - round(BUTTON_HEIGHT * 1.5)
-    for button in range(BUTTON_COUNT):
-        button_pos.append(
-            Rect(button_left + ((BUTTON_WIDTH + GAP) * button),
-                 button_top,
-                 BUTTON_WIDTH,
-                 BUTTON_HEIGHT)
-        )
+    def colour_for_status(self, status: TimerStatus) -> Tuple[int, int, int]:
+        return self.colour_map[status.value]
 
+    def collidepoint(self, pos: Tuple[int, int]):
+        if self.rect.collidepoint(pos[0], pos[1]):
+            self.action()
 
-def draw():
-    display.fill(WHITE)
-    # draw buttons
-    for button in range(BUTTON_COUNT):
-        rect = button_pos[button]
-        pygame.draw.rect(display, BLACK, rect, 3)
-        pygame.draw.rect(display, BUTTONS[button][1][timer_status], rect)
-        text = BUTTON_FONT.render(BUTTONS[button][0], 1, WHITE)
-        display.blit(text,
-                     (rect.centerx - text.get_width() // 2,
-                      rect.centery - text.get_height() // 2))
-    text = TIMER_FONT.render(f'{target_time // 60}:{target_time % 60:02}', 1, TICK_TEXT_COLOUR)
-    display.blit(text,
-                 (TICK_TEXT_CENTER[0] - text.get_width() // 2,
-                  TICK_TEXT_CENTER[1] - text.get_height() // 2))
-    offset = text.get_height()
-    if timer_status == TIMER_RUNNING:
-        text = MESSAGE_FONT.render(TIME_MESSAGES[pomodoro_status], 1, BLACK)
-    elif timer_status == TIMER_PAUSED:
-        text = MESSAGE_FONT.render("This isn't right you know!", 1, RED)
-    else:
-        text = MESSAGE_FONT.render('Waiting…', 1, GREEN)
-    display.blit(text,
-                 (TICK_TEXT_CENTER[0] - text.get_width() // 2,
-                  TICK_TEXT_CENTER[1] + offset - text.get_height() // 2))
-    pygame.display.update()
+    @property
+    def rect(self):
+        return Rect(self.button_pos.xy, (self.width, self.height))
 
 
-def main():
-    init_button_positions()
-    clock = pygame.time.Clock()
-    running = True
-    while running:
-        clock.tick(FPS)
-        draw()
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                running = False
-                continue
-            if event.type == pygame.MOUSEBUTTONDOWN:
-                pos = pygame.mouse.get_pos()
-                for n, button in enumerate(button_pos):
-                    if button.collidepoint(pos):
-                        (BUTTONS[n][2])()
-            if event.type == pygame.USEREVENT:
-                global target_time, pomodoro_status, pomodoro_rest_count
-                target_time -= 1 if target_time > 0 else 0
-                if target_time == 0:
-                    transition = TIME_TRANSITIONS[pomodoro_status]
-                    if len(transition) == 1:
-                        pomodoro_status = transition[0]
-                    else:
-                        if pomodoro_rest_count == LONG_REST_AFTER:
-                            pomodoro_status = transition[1]
-                            pomodoro_rest_count = 0
-                        else:
-                            pomodoro_status = transition[0]
-                            pomodoro_rest_count += 1
-                    target_time = TIMES[pomodoro_status]
+class Pomodoro:
+    # buttons
+    BUTTON_COUNT = 3
+    BUTTON_WIDTH, BUTTON_HEIGHT = 90, 40
+    BUTTON_GAP = 100
+    buttons = []
 
-    pygame.quit()
+    def __init__(self, work: int = 25, rest: int = 5, long_rest: int = 10):
+        # pygame essentials
+        pygame.init()
+        self.display = pygame.display.set_mode((WIDTH, HEIGHT))
+        pygame.display.set_caption('Living with tomatoes')
+        self.TIMES = {
+            'work': work,
+            'rest': rest,
+            'long_rest': long_rest
+        }
+        self.TIME_TRANSITIONS = {
+            'work': ['rest', 'long_rest'],
+            'rest': ['work'],
+            'long_rest': ['work']
+        }
+        self.TIME_MESSAGES = {
+            'work': 'Program! Program!',
+            'rest': 'Cool your heels!',
+            'long_rest': 'Get the coffee in!'
+        }
+        self.long_rest_after = 3
+        self.pomodoro_status = 'work'
+        self.pomodoro_rest_count = 0
+
+        # timing elements
+        self.target_time = self.TIMES[self.pomodoro_status] * scalar
+        self.timer_status = TimerStatus.TIMER_STOPPED
+        self.timer_start = self.target_time
+
+        # font declarations
+        self.BUTTON_FONT = pygame.freetype.SysFont('sans', 30)
+        self.TIMER_FONT = pygame.freetype.SysFont(pygame.freetype.get_default_font(), 100)
+        self.MESSAGE_FONT = pygame.freetype.SysFont('serif', 60)
+
+        # clock representation
+        self.clock_rect = Rect(10, 30, 80, 80)
+
+    def do_start(self):
+        if self.timer_status == TimerStatus.TIMER_RUNNING:
+            return
+        pygame.time.set_timer(pygame.USEREVENT, 1000)
+        self.timer_status = TimerStatus.TIMER_RUNNING
+
+    def do_pause(self):
+        if self.timer_status == TimerStatus.TIMER_RUNNING:
+            pygame.time.set_timer(pygame.USEREVENT, 0)
+            self.timer_status = TimerStatus.TIMER_PAUSED
+
+    def do_reset(self):
+        if self.timer_status == TimerStatus.TIMER_STOPPED:
+            return
+        pygame.time.set_timer(pygame.USEREVENT, 0)
+        self.pomodoro_status = 'work'
+        self.pomodoro_rest_count = 0
+        self.target_time = self.TIMES[self.pomodoro_status] * scalar
+        self.timer_status = TimerStatus.TIMER_STOPPED
+        self.timer_start = self.target_time
+
+    def init_button_positions(self):
+        button_left = (WIDTH - self.BUTTON_COUNT * self.BUTTON_WIDTH -
+                       self.BUTTON_GAP * (self.BUTTON_COUNT - 1)) // 2
+        button_top = HEIGHT - round(self.BUTTON_HEIGHT * 1.5)
+        button_pos = []
+        for button in range(self.BUTTON_COUNT):
+            button_pos.append(
+                Point(button_left + ((self.BUTTON_WIDTH + self.BUTTON_GAP) * button),
+                      button_top)
+            )
+        self.buttons.extend([
+            PomButton('START', (GREEN, GREY, GREEN), self.do_start, button_pos[0]),
+            PomButton('PAUSE', (GREY, BLUE, GREY), self.do_pause, button_pos[1]),
+            PomButton('RESET', (GREY, RED, RED), self.do_reset, button_pos[2])
+        ])
+
+    def draw(self):
+        self.display.fill(WHITE)
+        # draw buttons
+        for button_id in range(self.BUTTON_COUNT):
+            button = self.buttons[button_id]
+            rect = button.rect  # self.button_pos[button_id]
+            pygame.draw.rect(self.display, BLACK, rect, 3)
+            pygame.draw.rect(self.display, button.colour_for_status(self.timer_status), rect)
+            text = self.BUTTON_FONT.render(button.text, WHITE)
+            self.display.blit(text[0],
+                              (rect.centerx - text[1].width // 2,
+                               rect.centery - text[1].height // 2))
+        #  draw countdown
+        text = self.TIMER_FONT.render(f'{self.target_time // 60}:{self.target_time % 60:02}',
+                                      TICK_TEXT_COLOUR)
+        self.display.blit(text[0],
+                          (TICK_TEXT_CENTER.x - text[1].width // 2,
+                           TICK_TEXT_CENTER.y - text[1].height // 2))
+        offset = text[1].height
+        if self.timer_status == TimerStatus.TIMER_RUNNING:
+            text = self.MESSAGE_FONT.render(self.TIME_MESSAGES[self.pomodoro_status], BLACK)
+        elif self.timer_status == TimerStatus.TIMER_PAUSED:
+            text = self.MESSAGE_FONT.render("This isn't right you know!", RED)
+        else:
+            text = self.MESSAGE_FONT.render('Waiting…', GREEN)
+        self.display.blit(text[0],
+                          (TICK_TEXT_CENTER.x - text[1].width // 2,
+                           TICK_TEXT_CENTER.y + offset - text[1].height // 2))
+
+        # draw timer pie
+        if self.target_time > 0:
+            pygame.draw.arc(self.display,
+                            RED if self.pomodoro_status == 'work' else BLUE,
+                            self.clock_rect,
+                            math.radians(90-(360/(self.timer_start/self.target_time))),
+                            math.radians(90),
+                            5)
+        if self.pomodoro_rest_count < self.long_rest_after:
+            pygame.draw.arc(self.display,
+                            GREEN,
+                            self.clock_rect.inflate(-14, -14),
+                            math.radians(90-(360/(self.long_rest_after/(self.long_rest_after-self.pomodoro_rest_count)))),
+                            math.radians(90),
+                            5)
+
+        pygame.display.update()
+
+    def next_transition(self):
+        transition = self.TIME_TRANSITIONS[self.pomodoro_status]
+        if len(transition) == 1:
+            self.pomodoro_status = transition[0]
+        else:
+            if self.pomodoro_rest_count == self.long_rest_after:
+                self.pomodoro_status = transition[1]
+                self.pomodoro_rest_count = 0
+            else:
+                self.pomodoro_status = transition[0]
+                self.pomodoro_rest_count += 1
+        self.target_time = self.TIMES[self.pomodoro_status]
+        self.timer_start = self.target_time
+
+    def main(self):
+        self.init_button_positions()
+        clock = pygame.time.Clock()
+        running = True
+        while running:
+            clock.tick(FPS)
+            self.draw()
+            for event in pygame.event.get():
+                if event.type == pygame.QUIT:
+                    running = False
+                    continue
+                if event.type == pygame.MOUSEBUTTONDOWN:
+                    pos = pygame.mouse.get_pos()
+                    for button in self.buttons:
+                        button.collidepoint(pos)
+                if event.type == pygame.USEREVENT:
+                    self.target_time -= 1 if self.target_time > 0 else 0
+                    if self.target_time == 0:
+                        self.next_transition()
+        pygame.quit()
 
 
 if __name__ == '__main__':
-    main()
+    pom = Pomodoro()
+    pom.main()
