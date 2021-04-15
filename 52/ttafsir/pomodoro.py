c95332a9eb0ee4854b138df84acc4367f669e98c
Tafsir Thiam <ttafsir@gmail.com>
2021-04-14 23:56:51 -0400

    refactor to class

--- 52/ttafsir/pomodoro.py
+++ 52/ttafsir/pomodoro.py
@@ -1,6 +1,4 @@
 from rich.console import Console
-from rich.layout import Layout
-from rich.panel import Panel
 from rich.live import Live
 from time import sleep
 from datetime import timedelta
@@ -9,15 +7,34 @@ from datetime import timedelta
 console = Console()
 
 
-minutes = 25
-countdown_timer = timedelta(minutes=minutes)
+class PomodoroTimer:
+
+    def __init__(self, start_timer=25, break_timer=5):
+        self._timer = start_timer
+        self._break = break_timer
+        self._countdown = timedelta(minutes=self._timer)
+
+    def run(self, prompt="TIMER"):
+        with Live(auto_refresh=False) as live:
+            while self._countdown:
+                console.print(f"{prompt}: {self._countdown}", end="\r")
+                self._countdown += timedelta(seconds=-1)
+                sleep(1)
+                live.refresh()
+
+    def restart(self, start_timer, prompt):
+        self._countdown = timedelta(minutes=start_timer)
+        self.run(prompt)
 
 
-console.print(f"\nPOMODORO TIMER: {minutes}:00")
-with Live(auto_refresh=False) as live:
-    while countdown_timer:
-        console.print(str(countdown_timer), end="\r")
-        countdown_timer += timedelta(seconds=-1)
-        sleep(1)
-        live.refresh()
-    console.print("TIME IS UP!")
+timer_duration = int(input('Duration: ').strip())
+break_duration = int(input('Break Duration: ').strip())
+console.print("\nCTRL+C to quit")
+while True:
+    try:
+        timer = PomodoroTimer(start_timer=timer_duration)
+        timer.run()
+        timer.restart(start_timer=break_duration, prompt="BREAK")
+    except KeyboardInterrupt:
+        console.print("TIME'S UP")
+        break
