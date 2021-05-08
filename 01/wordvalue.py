8a78009f4d42ac722f75d0440b498a933f9e7690
TheeOx <eternity336@gmail.com>
2021-05-08 10:58:56 -0500

    Here you go! Problem 1 Solved!

--- 01/wordvalue.py
+++ 01/wordvalue.py
@@ -1,34 +1,27 @@
+import string
+
 from data import DICTIONARY, LETTER_SCORES
+
 
 def load_words():
     """Load dictionary into a list and return list"""
-    with open(DICTIONARY) as file:
-        contents = file.read()
-        words = contents.splitlines()
-    return words
+    f = open(DICTIONARY, 'r')
+    dictionary_list = [_.strip() for _ in f.readlines()]
+    f.close()
+    return dictionary_list
+
 
 def calc_word_value(word):
     """Calculate the value of the word entered into function
     using imported constant mapping LETTER_SCORES"""
-
-    letter_to_score = [LETTER_SCORES[letter.upper()] for letter in list(word)
-                                        if letter.upper() in LETTER_SCORES.keys()]
-    return sum(letter_to_score)
+    return sum(LETTER_SCORES[key] for key in word.upper() if key in string.ascii_uppercase)
 
 
-def max_word_value(list_of_words=None):
+def max_word_value(words=load_words()):
     """Calculate the word with the max value, can receive a list
     of words as arg, if none provided uses default DICTIONARY"""
-    if not list_of_words:
-        list_of_words = load_words()
-
-    word_to_score_list = [{'word': word, 'score': calc_word_value(word)}
-                                                for word in list_of_words]
-
-    sorted_words_to_score = sorted(word_to_score_list, key= lambda word_score: word_score['score'])
-
-    return sorted_words_to_score[-1]['word']
+    return sorted([(_, calc_word_value(_)) for _ in words], key=lambda a: a[1])[-1][0]
 
 
 if __name__ == "__main__":
-    pass # run unittests to validate
+    pass  # run unittests to validate
