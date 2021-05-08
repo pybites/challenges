0a88f91467cb542313883b795052b16073ca995a
TheeOx <eternity336@gmail.com>
2021-05-08 11:10:00 -0500

    Here you go! Problem 1 Solved! Trying this again cause i think I Uploaded wrong last time.

--- 01/test_wordvalue.py
+++ 01/test_wordvalue.py
@@ -1,6 +1,5 @@
 import unittest
 
-from data import DICTIONARY, LETTER_SCORES
 from wordvalue import load_words, calc_word_value, max_word_value
 
 TEST_WORDS = ('bob', 'julian', 'pybites', 'quit', 'barbeque')
