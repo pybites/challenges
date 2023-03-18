165763c98de0ce44377ab9ca88c2e080bc27da64
Pavel Ekshin <pavel.ekshine@gmail.com>
2023-03-19 00:23:46 +0300

    PCC13 pavelekshin fix tests

--- 13/pavelekshin/directors.py
+++ 13/pavelekshin/directors.py
@@ -30,14 +30,20 @@ def get_average_scores(directors):
     fd, dd = {}, {}
     Movie = namedtuple("Movie", "score movies")
     for k, v in directors.items():
-        if len(v) > MIN_MOVIES:
-            score = _calc_mean(v)
-            fd[k] = Movie(score=score, movies=v)
+        if not len(v) < MIN_MOVIES:
+            movies = _year_sort(v)
+            score = _calc_mean(movies)
+            fd[k] = Movie(score=score, movies=movies)
     sd = OrderedDict(
         {k: v for k, v in sorted(fd.items(), key=lambda x: x[1][0], reverse=True)}
     )
     return sd
 
+def _year_sort(movies):
+    for count, movie in enumerate(movies):
+        if not int(movie.year) > MIN_YEAR:
+            del (movies[count])
+    return movies
 
 def _calc_mean(movies):
     """Helper method to calculate mean of list of Movie namedtuples"""
@@ -51,11 +57,16 @@ def print_results(directors):
     """Print directors ordered by highest average rating. For each director
     print his/her movies also ordered by highest rated movie.
     See http://pybit.es/codechallenge13.html for example output"""
+    directors = dict(list(directors.items())[:10])
     fmt_director_entry = "{counter:02}. {director:<52} {avg}"
     fmt_movie_entry = "{year} {title:<50} {score}"
     sep_line = "-" * 60
     for count, (director, movies) in enumerate(directors.items(), 1):
-        print(fmt_director_entry.format(counter=count, director=director, avg=movies.score))
+        print(
+            fmt_director_entry.format(
+                counter=count, director=director, avg=movies.score
+            )
+        )
         print(sep_line)
         for title, year, score in sorted(
             movies.movies, key=lambda x: x[2], reverse=True
