ed2b958b0a9fdbe923642f76b662dad3f5aa9c7a
Tyler Z <zambam5ttv@gmail.com>
2023-07-20 22:35:04 -0500

    Changes based on feedback

--- 52/zambam5/timer.py
+++ 52/zambam5/timer.py
@@ -7,28 +7,30 @@ if platform.system() == "Windows":
     from winsound import Beep
 
 
-def pomodoro_timer(duration: int = 20, beep: bool = False):
+def pomodoro_timer(minutes: int = 20, beep: bool = False) -> None:
     """
-    Take a duration and begin a timer loop
+    Take a duration in minutes and sleep for that amount of time
     """
-    duration_delta = timedelta(minutes=duration)
-    d = datetime.now()
-    d_ = d + duration_delta
-    print("Starting timer " + str(d), "\r\nTimer will stop at " + str(d_))
-    sleep(duration_delta.seconds)
-    if beep:
+    minutes_delta = timedelta(minutes=minutes)
+    date_now = datetime.now()
+    date_after_minutes = date_now + minutes_delta
+    print(
+        "Starting timer " + str(date_now),
+        "\r\nTimer will stop at " + str(date_after_minutes),
+    )
+    sleep(minutes_delta.seconds)
+    if beep and platform.system() == "Windows":
         Beep(frequency=500, duration=50)
     pass
 
 
-def main():
+def main() -> None:
     if platform.system() == "Windows":
+        beep = False
         b_question = input(
             'Do you want the timer to beep when finished? Answer "yes" or "no" '
         )
         if b_question == "no":
-            beep = False
-        else:
             beep = True
     else:
         beep = False
@@ -37,9 +39,7 @@ def main():
         again = input(
             'Do you want to start the timer again? Answer "yes" or "no" only '
         )
-        if again == "yes":
-            continue
-        else:
+        if again == "no":
             print("Ending the loop, hope you got some good work done!")
             sleep(0.5)
             break
